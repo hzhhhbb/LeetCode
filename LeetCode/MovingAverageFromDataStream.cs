8588d32e7bc5a8c48016b13e436d5441c3e1cee9
Vincent <huangzihengengineer@gmail.com>
2020-01-08 00:46:21 +0800

    346. 数据流中的移动平均值

---
+++ LeetCode/MovingAverageFromDataStream.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    using System.Linq;
+
+    /// <summary>
+    /// 346. 数据流中的移动平均值
+    /// </summary>
+    public class MovingAverage
+    {
+        private Queue<int> Queue;
+
+        private int Size;
+        /** Initialize your data structure here. */
+        public MovingAverage(int size)
+        {
+            this.Queue = new Queue<int>(size);
+            this.Size = size;
+        }
+
+        public double Next(int val)
+        {
+            if (this.Queue.Count == this.Size)
+            {
+                this.Queue.Dequeue();
+            }
+            this.Queue.Enqueue(val);
+            return this.Queue.Average();
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -2,12 +2,25 @@
 
 namespace LeetCode
 {
+    using System.Collections;
+    using System.Collections.Generic;
+
     class Program
     {
         static void Main(string[] args)
         {
-            MyCircularQueue circularQueue = new MyCircularQueue(6); 
+            MyCircularQueue circularQueue = new MyCircularQueue(6);
+            Queue<int> queue = new Queue<int>(4);
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
 
+            foreach (int item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
             Console.WriteLine("should be true,{0}",circularQueue.EnQueue(6)); 
             Console.WriteLine("should be 6,{0}", circularQueue.Rear());  
             Console.WriteLine("should be 6,{0}", circularQueue.Rear()); 
