e71cea69cce66b82a8d1d22099ac048890fae300
vincent <huangzihengengineer@gmail.com>
2020-06-04 14:26:19 +0800

    200.岛屿数量

---
+++ LeetCode/NumberOfIslands.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    using System.Linq;
+
+    /// <summary>
+    /// 200.岛屿数量
+    /// </summary>
+    public class NumberOfIslands
+    {
+        public int NumIslands(char[][] grid)
+        {
+            if (grid == null || grid.Length == 0)
+            {
+                return 0;
+            }
+
+            int nr = grid.Length;
+            int nc = grid[0].Length;
+
+            int islandNum = 0;
+
+            Queue<Tuple<int, int>> neighbors = new Queue<Tuple<int, int>>();
+            for (int i = 0; i < nr; i++)
+            {
+                for (int j = 0; j < nc; j++)
+                {
+                    if (grid[i][j] == '1')
+                    {
+                        islandNum++;
+
+                        neighbors.Enqueue(new Tuple<int, int>(i, j));
+                        while (neighbors.Any())
+                        {
+                            var item = neighbors.Dequeue();
+                            if (item.Item1 >= 0 && item.Item1 < nr && item.Item2 >= 0 && item.Item2 < nc
+                                && grid[item.Item1][item.Item2] == '1')
+                            {
+
+                                grid[item.Item1][item.Item2] = '0';
+                                //上、下节点
+                                neighbors.Enqueue(new Tuple<int, int>(item.Item1 - 1, item.Item2));
+                                neighbors.Enqueue(new Tuple<int, int>(item.Item1 + 1, item.Item2));
+                                //左、右节点
+                                neighbors.Enqueue(new Tuple<int, int>(item.Item1, item.Item2 - 1));
+                                neighbors.Enqueue(new Tuple<int, int>(item.Item1, item.Item2 + 1));
+                            }
+                        }
+                    }
+                }
+            }
+
+            return islandNum;
+        }
+    }
+
+}
+
+    /*
+     * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+
+岛屿总是被水包围，并且每座岛屿只能由水平方向或竖直方向上相邻的陆地连接形成。
+
+此外，你可以假设该网格的四条边均被水包围。
+ 
+
+示例 1:
+
+输入:
+11110
+11010
+11000
+00000
+输出: 1
+示例 2:
+
+输入:
+11000
+11000
+00100
+00011
+输出: 3
+解释: 每座岛屿只能由水平和/或竖直方向上相邻的陆地连接而成。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/number-of-islands
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+/*
+ *# 200. 岛屿数量
+ * ## 审题
+ * 陆地是1，水是0，岛屿是单独的一个1，或者是1+1+1+1（相邻的1）。那么我们可以把1+1+1+1看成是整体的1。
+ * 碰到一个1，就把它相邻的1全找出来，岛屿数量+1。为了防止重复计数，需要把已访问过的陆地做一下标记，比如把陆地“吞并沉没”，即标记为0
+ * ## 实现思路
+ * 遍历二维数组，访问到1时，岛屿数量+1，并启动广度优先遍历，把遍历到的所有相邻的1标记为0
+ */
+
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -9,31 +9,51 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            MyCircularQueue circularQueue = new MyCircularQueue(6);
-            Queue<int> queue = new Queue<int>(4);
-            queue.Enqueue(1);
-            queue.Enqueue(2);
-            queue.Enqueue(3);
+            #region 622. 设计循环队列
 
-            foreach (int item in queue)
-            {
-                Console.WriteLine(item);
-            }
+            // {
+            //     MyCircularQueue circularQueue = new MyCircularQueue(6);
+            //     Queue<int> queue = new Queue<int>(4);
+            //     queue.Enqueue(1);
+            //     queue.Enqueue(2);
+            //     queue.Enqueue(3);
+            //
+            //     foreach (int item in queue)
+            //     {
+            //         Console.WriteLine(item);
+            //     }
+            //
+            //     Console.ReadKey();
+            //     Console.WriteLine("should be true,{0}", circularQueue.EnQueue(6));
+            //     Console.WriteLine("should be 6,{0}", circularQueue.Rear());
+            //     Console.WriteLine("should be 6,{0}", circularQueue.Rear());
+            //
+            //     Console.WriteLine("should be true,{0}", circularQueue.DeQueue());
+            //     Console.WriteLine("should be true,{0}", circularQueue.EnQueue(5));
+            //     Console.WriteLine("should be 5,{0}", circularQueue.Rear());
+            //     Console.WriteLine("should be true,{0}", circularQueue.DeQueue());
+            //     Console.WriteLine("should be -1,{0}", circularQueue.Front());
+            //     Console.WriteLine("should be false,{0}", circularQueue.DeQueue());
+            //     Console.WriteLine("should be false,{0}", circularQueue.DeQueue());
+            //     Console.WriteLine("should be false,{0}", circularQueue.DeQueue());
+            // }
 
-            Console.ReadKey();
-            Console.WriteLine("should be true,{0}",circularQueue.EnQueue(6)); 
-            Console.WriteLine("should be 6,{0}", circularQueue.Rear());  
-            Console.WriteLine("should be 6,{0}", circularQueue.Rear()); 
-    
-            Console.WriteLine("should be true,{0}", circularQueue.DeQueue());
-            Console.WriteLine("should be true,{0}", circularQueue.EnQueue(5));
-            Console.WriteLine("should be 5,{0}", circularQueue.Rear());
-            Console.WriteLine("should be true,{0}", circularQueue.DeQueue());
-            Console.WriteLine("should be -1,{0}",circularQueue.Front());
-            Console.WriteLine("should be false,{0}", circularQueue.DeQueue());
-            Console.WriteLine("should be false,{0}", circularQueue.DeQueue());
-            Console.WriteLine("should be false,{0}", circularQueue.DeQueue());
+            #endregion
 
+            #region 200. 岛屿数量
+
+            // {
+            //     char[][] grid = new char[][]
+            //                         {
+            //                             new char[5] { '1', '1', '0', '0', '0' },
+            //                             new char[5] { '1', '1', '0', '0', '0' },
+            //                             new char[5] { '0', '0', '1', '0', '0' },
+            //                             new char[5] { '0', '0', '0', '1', '1' },
+            //                         };
+            //     Console.WriteLine(new NumberOfIslands().NumIslands(grid));
+            // }
+
+            #endregion
 
             Console.ReadKey();
         }
