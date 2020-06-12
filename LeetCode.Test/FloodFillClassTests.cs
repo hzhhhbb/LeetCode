630e643dfb5f81672fa8d2ed678eb21485b46979
vincent <huangzihengengineer@gmail.com>
2020-06-12 15:00:19 +0800

    733. 图像渲染

---
+++ LeetCode.Test/FloodFillClassTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Tests
+{
+    [TestFixture()]
+    public class FloodFillClassTests
+    {
+        [Test()]
+        public void FloodFillTest()
+        {
+            int[][] image = new int[][] { new int[] { 1, 1, 1 }, new int[] { 1, 1, 0 }, new int[] { 1, 0, 1 }, };
+            int sr = 1;
+            int sc = 1;
+            int newColor = 2;
+            int[][] newImage = new int[][] { new int[] { 2, 2, 2 }, new int[] { 2, 2, 0 }, new int[] { 2, 0, 1 }, };
+
+            Assert.AreEqual(newImage,new FloodFillClass().FloodFill(image, sr,sc,newColor));
+
+            int[][] image1 = new int[][] { new int[] { 0, 0, 0 }, new int[] { 0, 1, 1 }};
+            int sr1 = 1;
+            int sc1 = 1;
+            int newColor1 = 1;
+            int[][] newImage1 = new int[][] { new int[] { 0, 0, 0 }, new int[] { 0, 1, 1 } };
+
+            Assert.AreEqual(newImage1, new FloodFillClass().FloodFill(image1, sr1, sc1, newColor1));
+
+
+
+        }
+    }
+}
---
+++ LeetCode/FloodFill.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    using System.Linq;
+
+    /// <summary>
+    /// 733. 图像渲染
+    /// </summary>
+    public class FloodFillClass
+    {
+        public int[][] FloodFill(int[][] image, int sr, int sc, int newColor)
+        {
+            int originColor = image[sr][sc];
+            Queue<Tuple<int, int>> node = new Queue<Tuple<int, int>>();
+
+            node.Enqueue(new Tuple<int, int>(sr, sc));
+            Dictionary<string,string> readNodeIndex=new Dictionary<string, string>();
+            while (node.Any())
+            {
+                var tempNode = node.Dequeue();
+                if (readNodeIndex.ContainsKey(string.Concat(tempNode.Item1, tempNode.Item2)))
+                {
+                    continue;
+                }
+
+                readNodeIndex.Add(string.Concat(tempNode.Item1,tempNode.Item2),string.Empty);
+
+                if (tempNode.Item1 >= 0 && tempNode.Item1 < image.Length
+                    && tempNode.Item2 >= 0 && tempNode.Item2 < image[0].Length
+                    && image[tempNode.Item1][tempNode.Item2] == originColor)
+                {
+                    image[tempNode.Item1][tempNode.Item2] = newColor;
+                    node.Enqueue(new Tuple<int, int>(tempNode.Item1 + 1, tempNode.Item2));
+                    node.Enqueue(new Tuple<int, int>(tempNode.Item1 - 1, tempNode.Item2));
+                    node.Enqueue(new Tuple<int, int>(tempNode.Item1, tempNode.Item2 + 1));
+                    node.Enqueue(new Tuple<int, int>(tempNode.Item1, tempNode.Item2 - 1));
+                }
+            }
+
+            return image;
+        }
+    }
+}
