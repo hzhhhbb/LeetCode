39322ff2aa4eb644d024505c28b334ace33d7b63
Vincent <huangzihengengineer@gmail.com>
2020-06-20 20:03:27 +0800

    14. 最长公共前缀

---
+++ src/LeetCode.Test/StringAndArray/LongestCommonPrefixClassTests.cs
@@ -0,0 +1,49 @@
+﻿using LeetCode.StringAndArray;
+using NUnit.Framework;
+
+namespace LeetCode.Tests.StringAndArray
+{
+    [TestFixture()]
+    public class LongestCommonPrefixClassTests
+    {
+        [Test()]
+        public void LongestCommonPrefixTest()
+        {
+            string[] strs={"flower","flow","flight"};
+
+            string commonPrefix = "fl";
+
+            Assert.AreEqual(commonPrefix,new LongestCommonPrefixClass().LongestCommonPrefix(strs));
+        }
+
+        [Test()]
+        public void LongestCommonPrefixTest1()
+        {
+            string[] strs={"dog","racecar","car"};
+
+            string commonPrefix = "";
+
+            Assert.AreEqual(commonPrefix,new LongestCommonPrefixClass().LongestCommonPrefix(strs));
+        }
+
+        [Test()]
+        public void LongestCommonPrefixTest2()
+        {
+            string[] strs={"dog"};
+
+            string commonPrefix = "dog";
+
+            Assert.AreEqual(commonPrefix,new LongestCommonPrefixClass().LongestCommonPrefix(strs));
+        }
+
+        [Test()]
+        public void LongestCommonPrefixTest3()
+        {
+            string[] strs={"aa","a"};
+
+            string commonPrefix = "a";
+
+            Assert.AreEqual(commonPrefix,new LongestCommonPrefixClass().LongestCommonPrefix(strs));
+        }
+    }
+}
---
+++ src/LeetCode/StringAndArray/LongestCommonPrefixClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.StringAndArray
+{
+    /// <summary>
+    /// 14. 最长公共前缀
+    /// </summary>
+    public class LongestCommonPrefixClass
+    {
+        public string LongestCommonPrefix(string[] strs) 
+        {
+            if (!strs.Any())
+            {
+                return "";
+            }
+
+            if (strs.Length == 1)
+            {
+                return strs[0];
+            }
+
+            List<char> chars=new List<char>();
+            
+            for (int columnIndex = 0; columnIndex < strs[0].Length; columnIndex++)
+            {
+                char tempStr = strs[0][columnIndex];
+                for (int rowIndex = 1; rowIndex < strs.Length; rowIndex++)
+                {
+                    if (strs[rowIndex].Length-1 < columnIndex)
+                    {
+                        return string.Join("", chars);
+                    }
+                    if(!tempStr.Equals(strs[rowIndex][columnIndex]))
+                    {
+                        return string.Join("", chars);
+                    }
+                }
+
+                chars.Add(tempStr);
+            }
+
+            return string.Join("", chars);
+        }
+    }
+}
+/*
+ * 可以把字符串数据看出是M*N的字符矩阵，纵向扫描矩阵即可
+ * 注意字符串长度不是一样的，所以需要防止在访问数组时越界
+ */
