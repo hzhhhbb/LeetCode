9968fbafb7d7345a48f703d72df91077eeb0acb9
Vincent <huangzihengengineer@gmail.com>
2020-06-21 20:32:19 +0800

    151. 翻转字符串里的单词

---
+++ src/LeetCode.Test/StringAndArray/ReverseWordsInAStringTests.cs
@@ -0,0 +1,65 @@
+﻿using LeetCode.StringAndArray;
+using NUnit.Framework;
+
+namespace LeetCode.Tests.StringAndArray
+{
+    [TestFixture()]
+    public class ReverseWordsInAStringTests
+    {
+        [Test()]
+        public void ReverseWordsTest()
+        {
+            string originStr = "the sky is blue";
+            string expected="blue is sky the";
+
+            string reversedStr =new ReverseWordsInAString().ReverseWords(originStr);
+
+            Assert.AreEqual(expected,reversedStr);
+        }
+
+        [Test()]
+        public void ReverseWordsTest1()
+        {
+            string originStr =  "  hello   world!  ";
+            string expected = "world! hello";
+
+            string reversedStr =new ReverseWordsInAString().ReverseWords(originStr);
+
+            Assert.AreEqual(expected,reversedStr);
+        }
+
+        [Test()]
+        public void ReverseWordsTest3()
+        {
+            string originStr =  " ";
+            string expected = "";
+
+            string reversedStr =new ReverseWordsInAString().ReverseWords(originStr);
+
+            Assert.AreEqual(expected,reversedStr);
+        }
+
+        [Test()]
+        public void ReverseWordsTest4()
+        {
+            string originStr =  "    aaaaa  ";
+            string expected = "aaaaa";
+
+            string reversedStr =new ReverseWordsInAString().ReverseWords(originStr);
+
+            Assert.AreEqual(expected,reversedStr);
+        }
+
+        [Test()]
+        public void ReverseWordsTest5()
+        {
+            string originStr =  "";
+            string expected = "";
+
+            string reversedStr =new ReverseWordsInAString().ReverseWords(originStr);
+
+            Assert.AreEqual(expected,reversedStr);
+        }
+
+    }
+}
---
+++ src/LeetCode/StringAndArray/ReverseWordsInAString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.StringAndArray
+{
+    /// <summary>
+    /// 151. 翻转字符串里的单词
+    /// </summary>
+   public class ReverseWordsInAString
+    {
+        public string ReverseWords(string s) 
+        {
+            return string.Join(" ", s.Split(" ").Reverse().Where(u=>!string.IsNullOrWhiteSpace(u)));
+        }
+    }
+}
+/*
+ * 1、将字符串按空格分割成字符串数组
+ * 2、反转字符串
+ * 3、使用string.join拼接字符串
+ */
