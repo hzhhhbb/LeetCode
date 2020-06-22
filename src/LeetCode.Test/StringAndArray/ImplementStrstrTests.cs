95f985cc3c4bae5edd0e776cea2eaa272d10b082
Vincent <huangzihengengineer@gmail.com>
2020-06-22 22:32:20 +0800

    28. 实现 strStr()

---
+++ src/LeetCode.Test/StringAndArray/ImplementStrstrTests.cs
@@ -0,0 +1,50 @@
+﻿using LeetCode.StringAndArray;
+using NUnit.Framework;
+
+namespace LeetCode.Tests.StringAndArray
+{
+    [TestFixture()]
+    public class ImplementStrstrTests
+    {
+        [Test()]
+        public void StrStrTest()
+        {
+            string haystack = "hello";
+            string needle = "ll";
+            int expectation = 2;
+
+            Assert.AreEqual(expectation,new ImplementStrstr().StrStr(haystack,needle));
+        }
+
+        
+        [Test()]
+        public void StrStrTest1()
+        {
+            string haystack = "aaaaa";
+            string needle = "bba";
+            int expectation = -1;
+
+            Assert.AreEqual(expectation,new ImplementStrstr().StrStr(haystack,needle));
+        }
+
+        [Test()]
+        public void StrStrTest2()
+        {
+            string haystack = "aaaaa";
+            string needle = "";
+            int expectation = 0;//我觉得这里应该返回-1，c#的indexof就是返回-1
+
+            Assert.AreEqual(expectation,new ImplementStrstr().StrStr(haystack,needle));
+        }
+
+        [Test()]
+        public void StrStrTest3()
+        {
+            string haystack = "mississippi";
+            string needle = "issip";
+            int expectation = 4;
+
+            Assert.AreEqual(expectation,new ImplementStrstr().StrStr(haystack,needle));
+        }
+    }
+}
---
+++ src/LeetCode/StringAndArray/ImplementStrstr.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.StringAndArray
+{
+    /// <summary>
+    /// 28. 实现 strStr()
+    /// </summary>
+   public class ImplementStrstr
+    {
+        public int StrStr(string haystack, string needle) 
+        {
+            if (string.IsNullOrEmpty(needle))
+            {
+                return 0;
+            }
+
+            if (needle.Length > haystack.Length)
+            {
+                return -1;
+            }
+
+            int result = -1;
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                if (haystack[i] == needle[0])
+                {
+                    int startIndex = i+1;
+                    int needleIndex = 1;
+
+                    while (startIndex<haystack.Length&&needleIndex<needle.Length)
+                    {
+                        if (haystack[startIndex] == needle[needleIndex])
+                        {
+                            startIndex++;
+                            needleIndex++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    if (needleIndex==needle.Length)
+                    {
+                        result=i;
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
