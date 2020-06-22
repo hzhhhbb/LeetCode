2bc581610ea3c3ad7d4b048ab195999dbaaa224a
Vincent <huangzihengengineer@gmail.com>
2020-06-23 00:02:11 +0800

    344. 反转字符串

---
+++ src/LeetCode.Test/StringAndArray/ReverseStringClassTests.cs
@@ -0,0 +1,31 @@
+﻿using LeetCode.StringAndArray;
+using NUnit.Framework;
+
+namespace LeetCode.Tests.StringAndArray
+{
+    [TestFixture()]
+    public class ReverseStringClassTests
+    {
+        [Test()]
+        public void ReverseStringTest()
+        {
+            char[] origin = {'h', 'e', 'l', 'l', 'o'};
+            char[] reversed = {'o','l','l','e','h'};
+
+            new ReverseStringClass().ReverseString(origin);
+
+            Assert.AreEqual(reversed,origin);
+        }
+
+        [Test()]
+        public void ReverseStringTest1()
+        {
+            char[] origin = {'H','a','n','n','a','h'};
+            char[] reversed = {'h','a','n','n','a','H'};
+
+            new ReverseStringClass().ReverseString(origin);
+
+            Assert.AreEqual(reversed,origin);
+        }
+    }
+}
---
+++ src/LeetCode/StringAndArray/ReverseStringClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.StringAndArray
+{
+    /// <summary>
+    /// 344. 反转字符串
+    /// </summary>
+   public class ReverseStringClass
+    {
+        public void ReverseString(char[] s)
+        {
+            if (s == null)
+            {
+                return;
+            }
+
+            int start = 0;
+            int end = s.Length - 1;
+
+            while (start < end)
+            {
+                s[start] = (char)(s[start] ^ s[end]);
+                s[end] = (char)(s[start] ^ s[end]);
+                s[start] = (char)(s[start] ^ s[end]);
+
+                start++;
+                end--;
+            }
+        }
+    }
+}
+/*
+ * 双指针，通过位异或运算实现原地交换（题目中限定数组中的所有字符都是 ASCII 码表中的可打印字符）
+ */
