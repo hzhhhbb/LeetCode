b347d6238ca45d5e8cd825de11f18e07eea1c1b8
Vincent <huangzihengengineer@gmail.com>
2020-06-23 23:34:18 +0800

    167. 两数之和 II - 输入有序数组

---
+++ src/LeetCode.Test/StringAndArray/TwoSumIIInputArrayIsSortedTests.cs
@@ -0,0 +1,42 @@
+﻿using LeetCode.StringAndArray;
+using NUnit.Framework;
+
+namespace LeetCode.Tests.StringAndArray
+{
+    [TestFixture()]
+    public class TwoSumIIInputArrayIsSortedTests
+    {
+        [Test()]
+        public void TwoSumTest()
+        {
+            int[] input = {2, 7, 11, 15};
+            int target = 9;
+
+            int[] expected = {1, 2};
+
+            Assert.AreEqual(expected,new TwoSumIIInputArrayIsSorted().TwoSum(input,target));
+        }
+
+        [Test()]
+        public void TwoSumTest1()
+        {
+            int[] input = {2, 7, 11, 15,30};
+            int target = 37;
+
+            int[] expected = {2, 5};
+
+            Assert.AreEqual(expected,new TwoSumIIInputArrayIsSorted().TwoSum(input,target));
+        }
+
+        [Test()]
+        public void TwoSumTest2()
+        {
+            int[] input = {-1,0};
+            int target = -1;
+
+            int[] expected = {1, 2};
+
+            Assert.AreEqual(expected,new TwoSumIIInputArrayIsSorted().TwoSum(input,target));
+        }
+    }
+}
---
+++ src/LeetCode/StringAndArray/TwoSumIIInputArrayIsSorted.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.StringAndArray
+{
+    /// <summary>
+    /// 167. 两数之和 II - 输入有序数组
+    /// </summary>
+   public class TwoSumIIInputArrayIsSorted
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            int start = 0;
+            int end = numbers.Length - 1;
+
+            while (start<end)
+            {
+                int sum = numbers[start] + numbers[end];
+                if (sum == target)
+                {
+                    return new int[2]{start+1,end+1};
+                }
+                else if(sum<target)
+                {
+                    start++;
+                }
+                else
+                {
+                    end--;
+                }
+            }
+
+            return new int[2]{-1, -1};
+        }
+    }
+}
