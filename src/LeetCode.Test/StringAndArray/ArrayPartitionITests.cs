9f37f1c245a4ee2e52cb7d4a7cdca73dc823c762
Vincent <huangzihengengineer@gmail.com>
2020-06-23 22:50:55 +0800

    561. 数组拆分 I

---
+++ src/LeetCode.Test/StringAndArray/ArrayPartitionITests.cs
@@ -0,0 +1,64 @@
+﻿using LeetCode.StringAndArray;
+using NUnit.Framework;
+
+namespace LeetCode.Tests.StringAndArray
+{
+    [TestFixture()]
+    public class ArrayPartitionITests
+    {
+        [Test()]
+        public void ArrayPairSumTest()
+        {
+            int[] input = {1, 4, 3, 2};
+            int expected = 4;
+
+            Assert.AreEqual(expected,new ArrayPartitionI().ArrayPairSum(input));
+        }
+
+        [Test()]
+        public void ArrayPairSumTest1()
+        {
+            int[] input = {-10, 5, -10, 10};
+            int expected = -5;
+
+            Assert.AreEqual(expected,new ArrayPartitionI().ArrayPairSum(input));
+        }
+
+        [Test()]
+        public void ArrayPairSumTest2()
+        {
+            int[] input = {-10, 5, -10, 10,-30,90};
+            int expected = -30;
+
+            Assert.AreEqual(expected,new ArrayPartitionI().ArrayPairSum2(input));
+        }
+
+
+        [Test()]
+        public void ArrayPairSum2Test()
+        {
+            int[] input = {1, 4, 3, 2};
+            int expected = 4;
+
+            Assert.AreEqual(expected,new ArrayPartitionI().ArrayPairSum2(input));
+        }
+
+        [Test()]
+        public void ArrayPairSum2Test1()
+        {
+            int[] input = {-10, 5, -10, 10};
+            int expected = -5;
+
+            Assert.AreEqual(expected,new ArrayPartitionI().ArrayPairSum2(input));
+        }
+
+        [Test()]
+        public void ArrayPairSum2Test2()
+        {
+            int[] input = {-10, 5, -10, 10,-30,90};
+            int expected = -30;
+
+            Assert.AreEqual(expected,new ArrayPartitionI().ArrayPairSum2(input));
+        }
+    }
+}
---
+++ src/LeetCode/StringAndArray/ArrayPartitionI.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.StringAndArray
+{
+    /// <summary>
+    /// 561. 数组拆分 I
+    /// </summary>
+   public class ArrayPartitionI
+    {
+        public int ArrayPairSum(int[] nums)
+        {
+            if (nums.Length == 2)
+            {
+                return Math.Min(nums[0], nums[1]);
+            }
+
+            nums = nums.OrderBy(u => u).ToArray();
+
+            int start = 0;
+            int end = nums.Length - 2;
+            int result = 0;
+
+            while (start<end)
+            {
+                result = result + nums[start] + nums[end];
+
+                start += 2;
+                end -= 2;
+            }
+
+            if (start == end)
+            {
+                result += nums[start];
+            }
+            
+            return result;
+        }
+
+        public int ArrayPairSum2(int[] nums)
+        {
+            nums = nums.OrderBy(u => u).ToArray();
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i+=2)
+            {
+                sum += nums[i];
+            }
+
+            return sum;
+        }
+    }
+}
+/*
+ * # 审题
+ * 题目中限定了数组数量是2n，那么数组数量一定是偶数,所以不需要考虑数组数量为奇数的情况
+ * 要求是求出最大值，那么就使每对的数最大，所以最小数必然要和第二小的数组成一对
+ * # 解法
+ * 两种解法，双指针和普通循环。相同点都是需要先按照升序排序，然后取两个数中的最小数相加即可
+ */
