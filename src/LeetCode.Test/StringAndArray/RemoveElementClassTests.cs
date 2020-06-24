b16b7c036e7ff32861f112332cb5506e8b7af3ea
Vincent <huangzihengengineer@gmail.com>
2020-06-25 00:58:04 +0800

    27. 移除元素

---
+++ src/LeetCode.Test/StringAndArray/RemoveElementClassTests.cs
@@ -0,0 +1,31 @@
+﻿using LeetCode.StringAndArray;
+using NUnit.Framework;
+
+namespace LeetCode.Tests.StringAndArray
+{
+    [TestFixture()]
+    public class RemoveElementClassTests
+    {
+        [Test()]
+        public void RemoveElementTest()
+        {
+            int[] nums = {3, 2, 2, 3};
+            int val = 3;
+
+            int expected = 2;
+            Assert.AreEqual(expected,new RemoveElementClass().RemoveElement(nums,val));
+            
+        }
+
+        [Test()]
+        public void RemoveElementTest1()
+        {
+            int[] nums = {0,1,2,2,3,0,4,2};
+            int val = 2;
+
+            int expected = 5;
+            Assert.AreEqual(expected,new RemoveElementClass().RemoveElement(nums,val));
+            
+        }
+    }
+}
---
+++ src/LeetCode/StringAndArray/RemoveElementClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.StringAndArray
+{
+    /// <summary>
+    /// 27. 移除元素
+    /// </summary>
+   public class RemoveElementClass
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int slow = 0;
+            for (int fast = 0; fast < nums.Length; fast++)
+            {
+                if (nums[fast] != val)
+                {
+                    nums[slow] = nums[fast];
+                    slow++;
+                }
+            }
+
+            return slow;
+        }
+    }
+}
