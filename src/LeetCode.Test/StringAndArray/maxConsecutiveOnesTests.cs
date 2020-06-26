5915f75a55986ab4e6070d5d22a251cc74babd65
Vincent <huangzihengengineer@gmail.com>
2020-06-26 23:38:55 +0800

    485. 最大连续1的个数

---
+++ src/LeetCode.Test/StringAndArray/maxConsecutiveOnesTests.cs
@@ -0,0 +1,64 @@
+﻿using LeetCode.StringAndArray;
+using NUnit.Framework;
+
+namespace LeetCode.Tests.StringAndArray
+{
+    [TestFixture()]
+    public class maxConsecutiveOnesTests
+    {
+        [Test()]
+        public void FindMaxConsecutiveOnesTest()
+        {
+            int[] nums = {1, 1, 0, 1, 1, 1};
+            int expected = 3;
+
+            Assert.AreEqual(expected,new maxConsecutiveOnes().FindMaxConsecutiveOnes(nums));
+        }
+
+        [Test()]
+        public void FindMaxConsecutiveOnesTest1()
+        {
+            int[] nums = {1, 1, 0, 1, 1, 1,0,0,0,1,1,1,1,0,1,1,1,1,1,1,1,1};
+            int expected = 8;
+
+            Assert.AreEqual(expected,new maxConsecutiveOnes().FindMaxConsecutiveOnes(nums));
+        }
+
+        [Test()]
+        public void FindMaxConsecutiveOnesTest2()
+        {
+            int[] nums = {1, 1};
+            int expected = 2;
+
+            Assert.AreEqual(expected,new maxConsecutiveOnes().FindMaxConsecutiveOnes(nums));
+        }
+
+        [Test()]
+        public void FindMaxConsecutiveOnesTest3()
+        {
+            int[] nums = {0, 0};
+            int expected = 0;
+
+            Assert.AreEqual(expected,new maxConsecutiveOnes().FindMaxConsecutiveOnes(nums));
+        }
+
+        [Test()]
+        public void FindMaxConsecutiveOnesTest4()
+        {
+            int[] nums = {0};
+            int expected = 0;
+
+            Assert.AreEqual(expected,new maxConsecutiveOnes().FindMaxConsecutiveOnes(nums));
+        }
+
+
+        [Test()]
+        public void FindMaxConsecutiveOnesTest5()
+        {
+            int[] nums = {1};
+            int expected = 1;
+
+            Assert.AreEqual(expected,new maxConsecutiveOnes().FindMaxConsecutiveOnes(nums));
+        }
+    }
+}
---
+++ src/LeetCode/StringAndArray/maxConsecutiveOnes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace LeetCode.StringAndArray
+{
+    /// <summary>
+    /// 485. 最大连续1的个数
+    /// </summary>
+    public class maxConsecutiveOnes
+    {
+        public int FindMaxConsecutiveOnes(int[] nums)
+        {
+            int maxCount = 0;
+            int tempCount = 0;
+            for (int fast = 0; fast < nums.Length; fast++)
+            {
+                if (nums[fast] == 1)
+                {
+                    tempCount++;
+                    maxCount = Math.Max(maxCount, tempCount);
+                }
+                else
+                {
+                    tempCount = 0;
+                }
+            }
+
+            return maxCount;
+        }
+    }
+}
