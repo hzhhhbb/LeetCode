e464c67dc74d30b249d8516adc0df2f7ad55fc5c
Vincent <huangzihengengineer@gmail.com>
2020-06-30 00:23:39 +0800

    209. 长度最小的子数组

---
+++ src/LeetCode.Test/StringAndArray/MinimumSizeSubarraySumTests.cs
@@ -0,0 +1,93 @@
+﻿using NUnit.Framework;
+using LeetCode.StringAndArray;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.StringAndArray.Tests
+{
+    [TestFixture()]
+    public class MinimumSizeSubarraySumTests
+    {
+        [Test()]
+        public void MinSubArrayLenTest()
+        {
+            int[] nums = { 2, 3, 1, 2, 4, 3 };
+            int sum = 7;
+            int expected = 2;
+
+            Assert.AreEqual(expected, new MinimumSizeSubarraySum().MinSubArrayLen(sum, nums));
+        }
+
+        [Test()]
+        public void MinSubArrayLenTest1()
+        {
+            int[] nums = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+            int sum = 13;
+            int expected = 0;
+
+            Assert.AreEqual(expected, new MinimumSizeSubarraySum().MinSubArrayLen(sum, nums));
+        }
+
+        [Test()]
+        public void MinSubArrayLenTest2()
+        {
+            int[] nums = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+            int sum = 12;
+            int expected = 12;
+
+            Assert.AreEqual(expected, new MinimumSizeSubarraySum().MinSubArrayLen(sum, nums));
+        }
+
+        [Test()]
+        public void MinSubArrayLenTest3()
+        {
+            int[] nums = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+            int sum = 11;
+            int expected = 11;
+
+            Assert.AreEqual(expected, new MinimumSizeSubarraySum().MinSubArrayLen(sum, nums));
+        }
+
+        [Test()]
+        public void MinSubArrayLenTest4()
+        {
+            int[] nums = { 2, 3, 1, 2, 4, 3 };
+            int sum = 4;
+            int expected = 1;
+
+            Assert.AreEqual(expected, new MinimumSizeSubarraySum().MinSubArrayLen(sum, nums));
+        }
+
+        [Test()]
+        public void MinSubArrayLenTest5()
+        {
+            int[] nums = { 2, 6, 1, 2, 4, 3 };
+            int sum = 8;
+            int expected = 2;
+
+            Assert.AreEqual(expected, new MinimumSizeSubarraySum().MinSubArrayLen(sum, nums));
+        }
+
+        [Test()]
+        public void MinSubArrayLen1Test()
+        {
+            int[] nums = { 2, 3, 1, 2, 4, 3 };
+            int sum = 7;
+            int expected = 2;
+
+            Assert.AreEqual(expected, new MinimumSizeSubarraySum().MinSubArrayLen1(sum, nums));
+        }
+
+        [Test()]
+        public void MinSubArrayLen1Test1()
+        {
+            int[] nums = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+            int sum = 11;
+            int expected = 11;
+
+            Assert.AreEqual(expected, new MinimumSizeSubarraySum().MinSubArrayLen1(sum, nums));
+        }
+    }
+}
---
+++ src/LeetCode/StringAndArray/MinimumSizeSubarraySum.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.StringAndArray
+{
+    /// <summary>
+    /// 209. 长度最小的子数组
+    /// </summary>
+   public class MinimumSizeSubarraySum
+    {
+        public int MinSubArrayLen(int s, int[] nums)
+        {
+            int minimumSize = int.MaxValue;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int sum = 0;
+                for (int j = i; j < nums.Length; j++)
+                {
+                    sum = sum + nums[j];
+                    if (sum >= s)
+                    {
+                        minimumSize = Math.Min(minimumSize, j - i + 1);
+                        break;
+                    }
+                }
+            }
+
+            return minimumSize == int.MaxValue ? 0 : minimumSize;
+        }
+
+        public int MinSubArrayLen1(int s, int[] nums)
+        {
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+
+            int start = 0;
+            int end = 0;
+            int minimumSize = int.MaxValue;
+            int sum = 0;
+            while (end<nums.Length)
+            {
+                sum += nums[end];
+                while (sum>=s)
+                {
+                    minimumSize = Math.Min(minimumSize, end - start + 1);
+                    sum = sum - nums[start];
+                    start++;
+                }
+
+                end++;
+            }
+
+            return minimumSize == int.MaxValue ? 0 : minimumSize;
+        }
+    }
+}
+/*
+ * 方法一：最简单的暴力
+ * 方法二：双指针
+ */
