949403203948439ac242ba5957a3f0c388a55035
vincent <huangzihengengineer@gmail.com>
2020-07-01 17:53:14 +0800

    557. 反转字符串中的单词 III

---
+++ src/LeetCode.Test/StringAndArray/ReverseWordsInAStringIIITests.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Tests.StringAndArray
+{
+    using LeetCode.StringAndArray;
+
+    using NUnit.Framework;
+
+    [TestFixture()]
+    public class ReverseWordsInAStringIIITests
+    {
+        [Test()]
+        public void ReverseWordsTest()
+        {
+            string input = "Let's take LeetCode contest";
+            string expected = "s'teL ekat edoCteeL tsetnoc";
+
+            Assert.AreEqual(expected,new ReverseWordsInAStringIII().ReverseWords(input));
+        }
+    }
+}
---
+++ src/LeetCode/StringAndArray/ReverseWordsInAStringIII.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.StringAndArray
+{
+    using System.Linq;
+
+    /// <summary>
+    /// 557. 反转字符串中的单词 III
+    /// </summary>
+    public class ReverseWordsInAStringIII
+    {
+        public string ReverseWords(string s)
+        {
+            return string.Join(" ", s.Split(" ").Select(u=>string.Join("",u.Reverse())));
+        }
+    }
+}
