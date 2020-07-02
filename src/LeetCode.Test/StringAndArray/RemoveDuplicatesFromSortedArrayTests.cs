6f4b8364f7a3226c8f21835457af70fe5cf85673
Vincent <huangzihengengineer@gmail.com>
2020-07-02 22:25:00 +0800

    26. 删除排序数组中的重复项

---
+++ src/LeetCode.Test/StringAndArray/RemoveDuplicatesFromSortedArrayTests.cs
@@ -0,0 +1,45 @@
+﻿using LeetCode.StringAndArray;
+using NUnit.Framework;
+
+namespace LeetCode.Tests.StringAndArray
+{
+    [TestFixture()]
+    public class RemoveDuplicatesFromSortedArrayTests
+    {
+        [Test()]
+        public void RemoveDuplicatesTest()
+        {
+            int[] input = {1, 1, 2};
+            int expected = 2;
+
+            Assert.AreEqual(expected,new RemoveDuplicatesFromSortedArray().RemoveDuplicates(input));
+        }
+
+        [Test()]
+        public void RemoveDuplicatesTest1()
+        {
+            int[] input = {0,0,1,1,1,2,2,3,3,4};
+            int expected = 5;
+
+            Assert.AreEqual(expected,new RemoveDuplicatesFromSortedArray().RemoveDuplicates(input));
+        }
+        
+        [Test()]
+        public void RemoveDuplicatesTest2()
+        {
+            int[] input = {};
+            int expected = 0;
+
+            Assert.AreEqual(expected,new RemoveDuplicatesFromSortedArray().RemoveDuplicates(input));
+        }
+
+        [Test()]
+        public void RemoveDuplicatesTest3()
+        {
+            int[] input = {0,0,0,1,1,1,1,1,1,1,1,2,2,3,4,5,6,7,7,8,9};
+            int expected = 10;
+
+            Assert.AreEqual(expected,new RemoveDuplicatesFromSortedArray().RemoveDuplicates(input));
+        }
+    }
+}
---
+++ src/LeetCode/StringAndArray/RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.StringAndArray
+{
+    /// <summary>
+    /// 26.删除排序数组中的重复项
+    /// </summary>
+    public class RemoveDuplicatesFromSortedArray
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+
+            int slow = 0;
+            for (int fast = 0; fast < nums.Length; fast++)
+            {
+                if (nums[fast] != nums[slow])
+                {
+                    slow++;
+                    nums[slow] = nums[fast];
+                }
+            }
+
+            return slow + 1;
+        }
+    }
+}
+/*
+ * 双指针
+ */
