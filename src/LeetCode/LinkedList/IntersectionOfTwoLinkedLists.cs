8508af2284398af1c37c688e0d80fe4d4ba9d2df
Vincent <huangzihengengineer@gmail.com>
2020-07-05 17:32:56 +0800

    160. 相交链表。哈希表法、双指针法

---
+++ src/LeetCode/LinkedList/IntersectionOfTwoLinkedLists.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace LeetCode.LinkedList
+{
+    /// <summary>
+    /// 160. 相交链表
+    /// </summary>
+   public class IntersectionOfTwoLinkedLists
+    {
+       //哈希表法
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            if (headA == null || headB == null)
+            {
+                return null;
+            }
+
+            HashSet<ListNode> a=new HashSet<ListNode>();
+            ListNode headANode = headA;
+            while (headANode!=null)
+            {
+                a.Add(headANode);
+                headANode = headANode.next;
+            }
+
+            ListNode headBNode = headB;
+            while (headBNode!=null)
+            {
+                if (a.Contains(headBNode))
+                {
+                    return headBNode;
+                }
+
+                headBNode = headBNode.next;
+            }
+
+            return null;
+        }
+
+        // 双指针法
+        public ListNode GetIntersectionNode1(ListNode headA, ListNode headB)
+        {
+            if (headA == null || headB == null)
+            {
+                return null;
+            }
+
+            ListNode tempHeadA = headA;
+            ListNode tempHeadB = headB;
+            bool aVisitedB = false;
+            bool bVisitedA = false;
+            while (tempHeadA!=tempHeadB)
+            {
+                if (tempHeadA == null)
+                {
+                    if (!aVisitedB)
+                    {
+                        tempHeadA = headB;
+                        aVisitedB = true;
+                    }
+                }
+                else
+                {
+                    tempHeadA = tempHeadA.next;
+                }
+
+                if (tempHeadB == null)
+                {
+                    if (!bVisitedA)
+                    {
+                        tempHeadB = headA;
+                        bVisitedA = true;
+                    }
+                }
+                else
+                {
+                    tempHeadB = tempHeadB.next;
+                }
+
+                if (tempHeadA == null && tempHeadB == null && aVisitedB && bVisitedA)
+                {
+                    break;
+                }
+
+            }
+
+            return tempHeadA == tempHeadB ? tempHeadA : null;
+        }
+    }
+}
