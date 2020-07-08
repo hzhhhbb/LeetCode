f50b45334830458aa8053a1c72580bc91f5499a2
Vincent <huangzihengengineer@gmail.com>
2020-07-08 23:59:43 +0800

    203. 移除链表元素

---
+++ src/LeetCode.Test/LinkedList/RemoveLinkedListElementsTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using LeetCode.LinkedList;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.LinkedList.Tests
+{
+    [TestFixture()]
+    public class RemoveLinkedListElementsTests
+    {
+        [Test()]
+        public void RemoveElementsTest()
+        {
+            // 删除中间结点
+            ListNode headA=new ListNode(1);
+            ListNode node2=new ListNode(2);
+            ListNode node3=new ListNode(3);
+            ListNode node4=new ListNode(4);
+            ListNode node5=new ListNode(5);
+            ListNode node6=new ListNode(6);
+            headA.next = node2;
+            node2.next = node3;
+            node3.next = node4;
+            node4.next = node5;
+            node5.next = node6;
+
+            ListNode headB=new ListNode(1);
+            ListNode nodeB2=new ListNode(2);
+            ListNode nodeB3=new ListNode(3);
+            ListNode nodeB4=new ListNode(4);
+            ListNode nodeB6=new ListNode(6);
+            headB.next = nodeB2;
+            nodeB2.next = nodeB3;
+            nodeB3.next = nodeB4;
+            nodeB4.next = nodeB6;
+
+            headA = new RemoveLinkedListElements().RemoveElements(headA, 5);
+            Assert.IsTrue(headA.IsEqual(headB));
+        }
+
+    }
+}
---
+++ src/LeetCode/LinkedList/RemoveLinkedListElements.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.LinkedList
+{
+   public class RemoveLinkedListElements
+    {
+        public ListNode RemoveElements(ListNode head, int val) 
+        {
+            if (head == null)
+            {
+                return head;
+            }
+
+            ListNode tempHead = new ListNode(0);
+            tempHead.next = head;
+
+            ListNode currentNode = tempHead;
+            while (currentNode.next!=null&&currentNode!=null)
+            {
+                if (currentNode.next.val == val)
+                {
+                    currentNode.next = currentNode.next.next;
+                }
+                else
+                {
+                    currentNode = currentNode.next;
+                }
+                
+            }
+
+            return tempHead.next;
+        }
+    }
+}
+/*
+ * 用一个哨兵节点当伪头，避免链表为空的情况出现
+ */
