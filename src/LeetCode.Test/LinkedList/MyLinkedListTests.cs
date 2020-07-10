856d8584992b757c6b49ac8ae1ce140cc9f0c2e8
vincent <huangzihengengineer@gmail.com>
2020-07-10 14:35:38 +0800

    707. 设计链表（双链表）

--- src/LeetCode.Test/LinkedList/MyLinkedListTests.cs
+++ src/LeetCode.Test/LinkedList/MyLinkedListTests.cs
@@ -41,5 +41,56 @@ namespace LeetCode.Tests.LinkedList
             linkedList.AddAtIndex(5,0);
             linkedList.AddAtHead(6);
         }
+
+        [Test()]
+        public void MyLinkedList2Test()
+        {
+            MyLinkedList2 linkedList = new MyLinkedList2();
+            linkedList.AddAtHead(1);
+            linkedList.AddAtTail(3);
+            linkedList.AddAtIndex(1, 2);
+
+            var expected = 2;
+            Assert.AreEqual(expected, linkedList.Get(1));
+
+            linkedList.DeleteAtIndex(1);
+
+            expected = 3;
+            Assert.AreEqual(expected, linkedList.Get(1));
+        }
+        [Test()]
+        public void MyLinkedList2Test1()
+        {
+            MyLinkedList2 linkedList = new MyLinkedList2();
+            linkedList.AddAtHead(7);
+            linkedList.AddAtHead(2);
+            linkedList.AddAtHead(1);
+            linkedList.AddAtIndex(3, 0);
+            linkedList.DeleteAtIndex(2);
+            linkedList.AddAtHead(6);
+            linkedList.AddAtTail(4);
+
+            var expected = 4;
+            Assert.AreEqual(expected, linkedList.Get(4));
+
+            linkedList.AddAtHead(4);
+            linkedList.AddAtIndex(5, 0);
+            linkedList.AddAtHead(6);
+        }
+
+        [Test()]
+        public void MyLinkedList2Test2()
+        {
+            MyLinkedList2 linkedList = new MyLinkedList2();
+            linkedList.AddAtHead(1);
+            linkedList.DeleteAtIndex(0);
+
+            var expected = 4;
+            Assert.AreEqual(expected, linkedList.Get(4));
+
+            linkedList.AddAtHead(4);
+            linkedList.AddAtIndex(5, 0);
+            linkedList.AddAtHead(6);
+        }
     }
 }
