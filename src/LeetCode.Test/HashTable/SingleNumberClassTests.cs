7b46ddf29b96a9ce2288793eb00f671f062068cc
Vincent <huangzihengengineer@gmail.com>
2020-07-19 01:12:07 +0800

    136. 只出现一次的数字

---
+++ src/LeetCode.Test/HashTable/SingleNumberClassTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq.Expressions;
+using LeetCode.HashTable;
+using NUnit.Framework;
+
+namespace LeetCode.Tests.HashTable
+{
+    [TestFixture()]
+    public class SingleNumberClassTests
+    {
+        [Test()]
+        public void SingleNumberTest()
+        { 
+            int[] input = new int[] {1, 2, 3, 1,2};
+            int expected = 3;
+            Assert.AreEqual(expected,new SingleNumberClass().SingleNumber(input));
+        }
+
+        [Test()]
+        public void SingleNumberTest1()
+        {
+            int[] input = new int[] {1, 2, 2, 3,3};
+            int expected = 1;
+            Assert.AreEqual(expected,new SingleNumberClass().SingleNumber(input));
+        }
+
+        [Test()]
+        public void SingleNumberTest2()
+        { 
+            int[] input = new int[] {1, 2, 3, 1,2};
+            int expected = 3;
+            Assert.AreEqual(expected,new SingleNumberClass().SingleNumber1(input));
+        }
+
+        [Test()]
+        public void SingleNumberTest3()
+        {
+            int[] input = new int[] {1, 2, 2, 3,3};
+            int expected = 1;
+            Assert.AreEqual(expected,new SingleNumberClass().SingleNumber1(input));
+        }
+    }
+}
---
+++ src/LeetCode/HashTable/SingleNumberClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace LeetCode.HashTable
+{
+    /// <summary>
+    /// 136. 只出现一次的数字
+    /// </summary>
+   public class SingleNumberClass
+    {
+        public int SingleNumber(int[] nums)
+        {
+            Array.Sort(nums);
+            int leftIndex = 0;
+            int rightIndex = 1;
+
+            while (leftIndex<nums.Length-1&&rightIndex<nums.Length-1)
+            {
+                if (nums[leftIndex] != nums[rightIndex])
+                {
+                    return nums[leftIndex];
+                }
+
+                leftIndex+=2;
+                rightIndex+=2;
+            }
+
+            return nums[leftIndex];
+        }
+        public int SingleNumber1(int[] nums)
+        {
+            int result = 0;
+            foreach (int num in nums)
+            {
+                result = result ^ num;
+            }
+
+            return result;
+        }
+    }
+}
+/*
+ * # 方法一（修改了原来的数组）
+ * 排序后用双指针
+ *
+ * # 方法二
+ * 异或运算满足交换律和结合律，全部数的异或运算结果则为只出现一次的数字
+ * a+b+a=b+a+a=b+(a+a)=b+0=b
+ */
