cc84f1800603b241ae3805c36b7fd7382e7cdf5d
Vincent <huangzihengengineer@gmail.com>
2020-07-19 01:34:22 +0800

    349. 两个数组的交集

---
+++ src/LeetCode.Test/HashTable/IntersectionOfTwoArraysTests.cs
@@ -0,0 +1,45 @@
+﻿using LeetCode.HashTable;
+using NUnit.Framework;
+
+namespace LeetCode.Tests.HashTable
+{
+    [TestFixture()]
+    public class IntersectionOfTwoArraysTests
+    {
+        [Test()]
+        public void IntersectionTest()
+        {
+            int[] nums1 = new int[] {1, 2, 3, 4};
+            int[] nums2 = new int[] {5, 6};
+            int[] expected = new int[] { };
+            Assert.AreEqual(expected, new IntersectionOfTwoArrays().Intersection(nums1, nums2));
+        }
+
+        [Test()]
+        public void IntersectionTest1()
+        {
+            int[] nums1 = new int[] {1, 2, 2, 1};
+            int[] nums2 = new int[] {2, 2};
+            int[] expected = new[] {2};
+            Assert.AreEqual(expected, new IntersectionOfTwoArrays().Intersection(nums1, nums2));
+        }
+
+        [Test()]
+        public void IntersectionTest2()
+        {
+            int[] nums1 = new int[] { };
+            int[] nums2 = new int[] {2, 2};
+            int[] expected = new int[] { };
+            Assert.AreEqual(expected, new IntersectionOfTwoArrays().Intersection(nums1, nums2));
+        }
+
+        [Test()]
+        public void IntersectionTest3()
+        {
+            int[] nums1 = new int[] {1};
+            int[] nums2 = new int[] { };
+            int[] expected = new int[] { };
+            Assert.AreEqual(expected, new IntersectionOfTwoArrays().Intersection(nums1, nums2));
+        }
+    }
+}
---
+++ src/LeetCode/HashTable/IntersectionOfTwoArrays.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.HashTable
+{
+    /// <summary>
+    /// 349. 两个数组的交集
+    /// </summary>
+    public class IntersectionOfTwoArrays
+    {
+        public int[] Intersection(int[] nums1, int[] nums2)
+        {
+            if (!nums1.Any() || !nums2.Any())
+            {
+                return new int[0];
+            }
+
+            HashSet<int> result=new  HashSet<int>();
+            HashSet<int> set1=new HashSet<int>(nums1);
+            foreach (int num in nums2)
+            {
+                if (set1.Contains(num))
+                {
+                    result.Add(num);
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
+/*
+ * 使用两个HashSet即可
+ */
