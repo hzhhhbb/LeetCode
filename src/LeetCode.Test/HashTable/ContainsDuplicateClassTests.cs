9d95e17cbfff5eb7e70427d7f9912fed94d94a8c
Vincent <huangzihengengineer@gmail.com>
2020-07-19 00:44:47 +0800

    217. 存在重复元素

---
+++ src/LeetCode.Test/HashTable/ContainsDuplicateClassTests.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode.HashTable;
+using NUnit.Framework;
+
+namespace LeetCode.Tests.HashTable
+{
+    [TestFixture()]
+    public class ContainsDuplicateClassTests
+    {
+        [Test()]
+        public void ContainsDuplicateTest()
+        {
+            int[] input = new int[] {1, 2, 3, 1};
+            bool expected = true;
+            Assert.AreEqual(expected, new ContainsDuplicateClass().ContainsDuplicate(input));
+        }
+
+        [Test()]
+        public void ContainsDuplicateTest1()
+        {
+            int[] input = new int[] {1, 2, 3, 4};
+            bool expected = false;
+            Assert.AreEqual(expected, new ContainsDuplicateClass().ContainsDuplicate(input));
+        }
+
+        [Test()]
+        public void ContainsDuplicateTest2()
+        {
+            int[] input = new int[] { };
+            bool expected = false;
+            Assert.AreEqual(expected, new ContainsDuplicateClass().ContainsDuplicate(input));
+        }
+
+        [Test()]
+        public void ContainsDuplicateTest3()
+        {
+            int[] input = null;
+            bool expected = false;
+            Assert.AreEqual(expected, new ContainsDuplicateClass().ContainsDuplicate(input));
+        }
+    }
+}
---
+++ src/LeetCode/HashTable/ContainsDuplicateClass.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.HashTable
+{
+    /// <summary>
+    /// 217. 存在重复元素
+    /// </summary>
+   public class ContainsDuplicateClass
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+            {
+                return false;
+            }
+
+            HashSet<int> set=new HashSet<int>(nums.Length);
+            foreach (int num in nums)
+            {
+                if (set.Contains(num))
+                {
+                    return true;
+                }
+                else
+                {
+                    set.Add(num);
+                }
+            }
+
+            return false;
+        }
+    }
+}
+/*# 方法一
+ * 使用哈希集合即可
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(n)
+ *# 方法二（简单未实现）
+ * 先排序，如果存在重复，那么相邻的数字必定相等
+ */
