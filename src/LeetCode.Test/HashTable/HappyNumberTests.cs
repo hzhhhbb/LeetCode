842f706cb93857012cab1529c538efc64b634868
Vincent <huangzihengengineer@gmail.com>
2020-07-19 13:34:56 +0800

    202. 快乐数

---
+++ src/LeetCode.Test/HashTable/HappyNumberTests.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode.HashTable;
+using NUnit.Framework;
+
+namespace LeetCode.Tests.HashTable
+{
+    [TestFixture()]
+    public class HappyNumberTests
+    {
+        [Test()]
+        public void IsHappyTest()
+        {
+            int input = 19;
+            bool expected = true;
+            Assert.AreEqual(expected,new HappyNumber().IsHappy(input));
+        }
+
+        [Test()]
+        public void IsHappyTest1()
+        {
+            int input = 0;
+            bool expected = false;
+            Assert.AreEqual(expected,new HappyNumber().IsHappy(input));
+        }
+
+        [Test()]
+        public void IsHappyTest2()
+        {
+            int input = 1;
+            bool expected = true;
+            Assert.AreEqual(expected,new HappyNumber().IsHappy(input));
+        }
+    }
+}
---
+++ src/LeetCode/HashTable/HappyNumber.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.HashTable
+{
+    /// <summary>
+    /// 202. 快乐数
+    /// </summary>
+   public class HappyNumber
+    {
+        public bool IsHappy(int n)
+        {
+            HashSet<int> visited=new HashSet<int>();
+            visited.Add(n);
+            while (n!=1)
+            {
+                n= this.GetNextNumber(n);
+                if (visited.Contains(n))
+                {
+                    return false;
+                }
+                else
+                {
+                    visited.Add(n);
+                }
+            }
+
+            return true;
+        }
+
+        private int GetNextNumber(int n)
+        {
+            int sum = 0;
+            while (n>0)
+            {
+                int d = n % 10;
+                n = n / 10;
+                sum += d * d;
+            }
+
+            return sum;
+        }
+    }
+}
+/*
+ * 用哈希表存储已经遍历过的数，来确定是否进入无限循环
+ */
