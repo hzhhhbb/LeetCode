59fc9b7290f11064ad8e0b78d7f9fe1b015f761d
Vincent <huangzihengengineer@gmail.com>
2020-07-20 20:54:42 +0800

    1. 两数之和

---
+++ src/LeetCode.Test/HashTable/TwoSumClassTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using LeetCode.HashTable;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Tests.HashTable
+{
+    [TestFixture()]
+    public class TwoSumClassTests
+    {
+        [Test()]
+        public void TwoSumTest()
+        {
+            int[] input=new int[]{2, 7, 11, 15};
+            int target = 9;
+
+            int[] expected=new int[]{0,1};
+            Assert.AreEqual(expected,new TwoSumClass().TwoSum(input,target));
+        }
+    }
+}
---
+++ src/LeetCode/HashTable/TwoSumClass.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.HashTable
+{
+    /// <summary>
+    /// 1. 两数之和
+    /// </summary>
+    public class TwoSumClass
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> kv = new Dictionary<int, int>(nums.Length - 1);
+            for (var index = 0; index < nums.Length; index++)
+            {
+                int num = nums[index];
+                if (kv.ContainsKey(target - num))
+                {
+                    return new int[] {kv[target - num], index};
+                }
+
+                kv.TryAdd(num, index);
+            }
+
+            return new int[] { };
+        }
+    }
+}
