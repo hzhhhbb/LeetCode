b32f999e982e2a26047bedc5dcd462e6f2689760
Vincent <huangzihengengineer@gmail.com>
2020-07-20 23:18:35 +0800

    205. 同构字符串

---
+++ src/LeetCode.Test/HashTable/IsomorphicStringsTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using LeetCode.HashTable;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Tests.HashTable
+{
+    [TestFixture()]
+    public class IsomorphicStringsTests
+    {
+        [Test()]
+        public void IsIsomorphicTest()
+        {
+            //同构字符串
+            string str1 = "egg";
+            string str2 = "add";
+            Assert.IsTrue(new IsomorphicStrings().IsIsomorphic(str1,str2));
+        }
+
+        [Test()]
+        public void IsIsomorphicTest1()
+        {
+            //非同构字符串
+            string str1 = "foo";
+            string str2 = "bar";
+            Assert.IsFalse(new IsomorphicStrings().IsIsomorphic(str1,str2));
+        }
+        [Test()]
+        public void IsIsomorphicTest3()
+        {
+            //非同构字符串
+            string str1 = "bar";
+            string str2 = "foo";
+            Assert.IsFalse(new IsomorphicStrings().IsIsomorphic(str1,str2));
+        }
+        [Test()]
+        public void IsIsomorphicTest2()
+        {
+            //同构字符串
+            string str1 = "paper";
+            string str2 = "title";
+            Assert.IsTrue(new IsomorphicStrings().IsIsomorphic(str1,str2));
+        }
+        [Test()]
+        public void IsIsomorphicTest4()
+        {
+            //非同构字符串
+            string str1 = "ab";
+            string str2 = "aa";
+            Assert.IsFalse(new IsomorphicStrings().IsIsomorphic(str1,str2));
+        }
+    }
+}
---
+++ src/LeetCode/HashTable/IsomorphicStrings.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.HashTable
+{
+    /// <summary>
+    /// 205. 同构字符串
+    /// </summary>
+    public class IsomorphicStrings
+    {
+        public bool IsIsomorphic(string s, string t) 
+        {
+            Dictionary<char,char> kv=new Dictionary<char,char>();
+            Dictionary<char,char> kv1=new Dictionary<char,char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (kv.ContainsKey(s[i]))
+                {
+                    if (kv[s[i]] != t[i])
+                    {
+                        return false;
+                    }
+                }
+                else if(kv1.ContainsKey(t[i]))
+                {
+                    if (kv1[t[i]]!=s[i])
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    kv.Add(s[i],t[i]);
+                    kv1.Add(t[i],s[i]);
+                }
+            }
+
+            return true;
+        }
+    }
+}
