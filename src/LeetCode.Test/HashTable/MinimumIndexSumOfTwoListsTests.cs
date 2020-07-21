aae2bbf1f166a59a4292640c00dc30d025ee7434
Vincent <huangzihengengineer@gmail.com>
2020-07-21 22:39:18 +0800

    599. 两个列表的最小索引总和

---
+++ src/LeetCode.Test/HashTable/MinimumIndexSumOfTwoListsTests.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using LeetCode.HashTable;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Tests.HashTable
+{
+    [TestFixture()]
+    public class MinimumIndexSumOfTwoListsTests
+    {
+        [Test()]
+        public void FindRestaurantTest()
+        {
+            string[] list1=new string[]{"Shogun", "Tapioca Express", "Burger King", "KFC"};
+            string[] list2=new string[]{"Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"};
+
+            string[] expected=new string[]{"Shogun"};
+
+            Assert.AreEqual(expected,new MinimumIndexSumOfTwoLists().FindRestaurant(list1,list2));
+        }
+
+        [Test()]
+        public void FindRestaurantTest1()
+        {
+            string[] list1=new string[]{"Shogun", "Tapioca Express", "Burger King", "KFC"};
+            string[] list2=new string[]{"KFC", "Shogun", "Burger King"};
+
+            string[] expected=new string[]{"Shogun"};
+
+            Assert.AreEqual(expected,new MinimumIndexSumOfTwoLists().FindRestaurant(list1,list2));
+        }
+
+        [Test()]
+        public void FindRestaurantTest2()
+        {
+            string[] list1=new string[]{};
+            string[] list2=new string[]{"KFC", "Shogun", "Burger King"};
+
+            string[] expected=new string[]{};
+
+            Assert.AreEqual(expected,new MinimumIndexSumOfTwoLists().FindRestaurant(list1,list2));
+        }
+
+        [Test()]
+        public void FindRestaurantTest3()
+        {
+            string[] list1=new string[]{"Shogun", "Tapioca Express", "Burger King", "KFC"};
+            string[] list2=new string[]{};
+
+            string[] expected=new string[]{};
+
+            Assert.AreEqual(expected,new MinimumIndexSumOfTwoLists().FindRestaurant(list1,list2));
+        }
+
+        [Test()]
+        public void FindRestaurantTest4()
+        {
+            // 多个答案
+            string[] list1=new string[]{"Shogun", "Tapioca Express", "Burger King", "KFC"};
+            string[] list2=new string[]{"Tapioca Express", "Shogun", "Burger King", "KFC"};
+
+            string[] expected=new string[]{"Tapioca Express", "Shogun"};
+
+            Assert.AreEqual(expected,new MinimumIndexSumOfTwoLists().FindRestaurant(list1,list2));
+        }
+    }
+}
---
+++ src/LeetCode/HashTable/MinimumIndexSumOfTwoLists.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.HashTable
+{
+    /// <summary>
+    /// 599. 两个列表的最小索引总和
+    /// </summary>
+    public class MinimumIndexSumOfTwoLists
+    {
+        public string[] FindRestaurant(string[] list1, string[] list2) 
+        {
+            if (!list1.Any() || !list2.Any())
+            {
+                return new string[]{};
+            }
+
+            Dictionary<string,int> dic=new Dictionary<string, int>();
+            for (int i = 0; i < list1.Length; i++)
+            {
+                dic.TryAdd(list1[i], i);
+            }
+
+            Dictionary<string,int> resultDic=new Dictionary<string, int>();
+
+            for (var index = 0; index < list2.Length; index++)
+            {
+                string s = list2[index];
+                if (dic.ContainsKey(s))
+                {
+                    resultDic.TryAdd(s, index + dic[s]);
+                }
+            }
+
+            List<string> result=new List<string>(list1.Length);
+            var minResult = resultDic.Min(u => u.Value);
+            foreach (KeyValuePair<string, int> kv in resultDic)
+            {
+                if (kv.Value == minResult)
+                {
+                    result.Add(kv.Key);
+                }
+            }
+
+
+            return result.ToArray();
+        }
+    }
+}
