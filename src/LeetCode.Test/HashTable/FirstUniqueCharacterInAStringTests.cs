54f493695f488742924ae346fa2f77c50784fdcb
Vincent <huangzihengengineer@gmail.com>
2020-07-21 23:00:13 +0800

    387. 字符串中的第一个唯一字符

---
+++ src/LeetCode.Test/HashTable/FirstUniqueCharacterInAStringTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using LeetCode.HashTable;
+
+namespace LeetCode.Tests.HashTable
+{
+    [TestFixture()]
+    public class FirstUniqueCharacterInAStringTests
+    {
+        [Test()]
+        public void FirstUniqCharTest()
+        {
+            string input = "leetcode";
+            int expected = 0;
+
+            Assert.AreEqual(expected,new FirstUniqueCharacterInAString().FirstUniqChar(input));
+        }
+
+        [Test()]
+        public void FirstUniqCharTest1()
+        {
+            string input = "loveleetcode";
+            int expected = 2;
+
+            Assert.AreEqual(expected,new FirstUniqueCharacterInAString().FirstUniqChar(input));
+        }
+
+        [Test()]
+        public void FirstUniqCharTest2()
+        {
+            string input = "lllllllllll";
+            int expected = -1;
+
+            Assert.AreEqual(expected,new FirstUniqueCharacterInAString().FirstUniqChar(input));
+        }
+
+        [Test()]
+        public void FirstUniqCharTest3()
+        {
+            string input = "luluiliooi";
+            int expected = -1;
+
+            Assert.AreEqual(expected,new FirstUniqueCharacterInAString().FirstUniqChar(input));
+        }
+    }
+}
---
+++ src/LeetCode/HashTable/FirstUniqueCharacterInAString.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.HashTable
+{
+    /// <summary>
+    /// 387. 字符串中的第一个唯一字符
+    /// </summary>
+   public class FirstUniqueCharacterInAString
+    {
+        public int FirstUniqChar(string s) 
+        {
+            // 每个字符和它出现的次数、索引
+            Dictionary<char,Tuple<int,int>> dic=new Dictionary<char,Tuple<int,int>>(s.Length);
+         
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (dic.ContainsKey(s[i]))
+                {
+                    dic[s[i]] = new Tuple<int, int>(dic[s[i]].Item1+1,dic[s[i]].Item2);
+                }
+                else
+                {
+                    dic.Add(s[i],new Tuple<int, int>(1,i));
+                }
+            }
+
+            foreach (KeyValuePair<char, Tuple<int,int>> kv in dic)
+            {
+                if (kv.Value.Item1 == 1)
+                {
+                    return kv.Value.Item2;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
