91d423cd09bdbe34c2073f7b150189c240cd421a
Vincent <huangzihengengineer@gmail.com>
2020-07-23 21:20:07 +0800

    49. 字母异位词分组

---
+++ src/LeetCode.Test/HashTable/GroupAnagramsClassTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using LeetCode.HashTable;
+using System.Collections.Generic;
+
+namespace LeetCode.Tests.HashTable
+{
+    [TestFixture()]
+    public class GroupAnagramsClassTests
+    {
+        [Test()]
+        public void GroupAnagramsTest()
+        {
+            string[] input = {"eat", "tea", "tan", "ate", "nat", "bat"};
+            List<List<string>> expected = new List<List<string>>();
+            expected.Add(new List<string>(){"eat","tea","ate"});
+            expected.Add(new List<string>(){"tan","nat"});
+            expected.Add(new List<string>(){"bat"});
+
+            Assert.AreEqual(expected,new GroupAnagramsClass().GroupAnagrams(input));
+        }
+    }
+}
---
+++ src/LeetCode/HashTable/GroupAnagramsClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.HashTable
+{
+    /// <summary>
+    /// 49. 字母异位词分组
+    /// </summary>
+    public class GroupAnagramsClass
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs) 
+        {
+            if (strs.Length == 0)
+            {
+                return new List<IList<string>>();
+            }
+            Dictionary<string,IList<string>> result=new Dictionary<string, IList<string>>();
+
+            foreach (string str in strs)
+            {
+                var s = str.ToCharArray();
+                Array.Sort(s);
+                string key = string.Join("", s);
+                if (result.ContainsKey(key))
+                {
+                    if (result[key] == null)
+                    {
+                        result[key]=new List<string>(){str};
+                    }
+                    else
+                    {
+                        result[key].Add(str);
+                    }
+                }
+                else
+                {
+                    result.Add(key,new List<string>(){str});
+                }
+            }
+
+            return result.Select(u => u.Value).ToList();
+        }
+    }
+}
