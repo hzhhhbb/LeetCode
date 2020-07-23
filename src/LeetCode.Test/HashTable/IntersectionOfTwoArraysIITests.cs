a7238343ec3dafeacc4683cceb8e8fc44a6cea97
vincent <huangzihengengineer@gmail.com>
2020-07-23 14:45:25 +0800

    350. 两个数组的交集 II

---
+++ src/LeetCode.Test/HashTable/IntersectionOfTwoArraysIITests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using LeetCode.HashTable;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Tests.HashTable
+{
+    [TestFixture()]
+    public class IntersectionOfTwoArraysIITests
+    {
+        [Test()]
+        public void IntersectTest()
+        {
+            int[] nums1=new int[]{1,2,2,1};
+            int[] nums2=new int[]{2,2};
+            int[] expected=new int[]{2,2};
+
+            Assert.AreEqual(expected,new IntersectionOfTwoArraysII().Intersect(nums1,nums2));
+        }
+
+        [Test()]
+        public void IntersectTest1()
+        {
+            int[] nums1=new int[]{ 4, 9, 5 };
+            int[] nums2=new int[]{ 9, 4, 9, 8, 4 };
+            int[] expected=new int[]{ 9,4 };
+
+            Assert.AreEqual(expected,new IntersectionOfTwoArraysII().Intersect(nums1,nums2));
+        }
+
+
+        [Test()]
+        public void IntersectTest2()
+        {
+            int[] nums1 = new int[] { };
+            int[] nums2 = new int[] { 9, 4, 9, 8, 4 };
+            int[] expected = new int[] { };
+
+            Assert.AreEqual(expected, new IntersectionOfTwoArraysII().Intersect(nums1, nums2));
+        }
+
+        [Test()]
+        public void IntersectTest3()
+        {
+            int[] nums1 = new int[] { 9, 4, 9, 8, 4 };
+            int[] nums2 = new int[] {  };
+            int[] expected = new int[] { };
+
+            Assert.AreEqual(expected, new IntersectionOfTwoArraysII().Intersect(nums1, nums2));
+        }
+    }
+}
---
+++ src/LeetCode/HashTable/IntersectionOfTwoArraysII.cs
@@ -0,0 +1,48 @@
+﻿namespace LeetCode.HashTable
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// 350. 两个数组的交集 II
+    /// </summary>
+    public class IntersectionOfTwoArraysII
+    {
+        public int[] Intersect(int[] nums1, int[] nums2)
+        {
+            if (nums2.Length < nums1.Length)
+            {
+                return this.Intersect(nums2, nums1);
+            }
+
+            // 统计nums1数字出现的次数
+            Dictionary<int,int> numAndTimes=new Dictionary<int, int>();
+            foreach (int i in nums1)
+            {
+                if (numAndTimes.ContainsKey(i))
+                {
+                    numAndTimes[i] = numAndTimes[i] + 1;
+                }
+                else
+                {
+                    numAndTimes.Add(i,1);
+                }
+            }
+
+            List<int> repeatedNum=new List<int>();
+            foreach (int i in nums2)
+            {
+                if (numAndTimes.ContainsKey(i))
+                {
+                    if (numAndTimes[i] > 0)
+                    {
+                        repeatedNum.Add(i);
+                        numAndTimes[i] = numAndTimes[i] - 1;
+                    }
+                }
+            }
+
+            return repeatedNum.ToArray();
+        }
+    }
+}
