1e874a71d1a79c1eaa4eb1c05559f4adc2c42022
Vincent <huangzihengengineer@gmail.com>
2020-07-24 22:46:09 +0800

    36. 有效的数独

---
+++ src/LeetCode.Test/HashTable/ValidSudokuTests.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using LeetCode.HashTable;
+
+namespace LeetCode.Tests.HashTable
+{
+    [TestFixture()]
+    public class ValidSudokuTests
+    {
+        [Test()]
+        public void IsValidSudokuTest()
+        {
+            char[][] input=new char[][]
+            {
+                new char[]{'5','3','.','.','7','.','.','.','.'}, 
+                new char[]{'6','.','.','1','9','5','.','.','.'}, 
+                new char[]{'.','9','8','.','.','.','.','6','.'}, 
+                new char[]{'8','.','.','.','6','.','.','.','3'}, 
+                new char[]{'4','.','.','8','.','3','.','.','1'}, 
+                new char[]{'7','.','.','.','2','.','.','.','6'}, 
+                new char[]{'.','6','.','.','.','.','2','8','.'}, 
+                new char[]{'.','.','.','4','1','9','.','.','5'}, 
+                new char[]{'.','.','.','.','8','.','.','7','9'}, 
+            };
+            bool expected = true;
+            Assert.AreEqual(expected,new ValidSudoku().IsValidSudoku(input));
+        }
+
+        [Test()]
+        public void IsValidSudokuTest1()
+        {
+            char[][] input=new char[][]
+            {
+                new char[]{'8','3','.','.','7','.','.','.','.'}, 
+                new char[]{'6','.','.','1','9','5','.','.','.'}, 
+                new char[]{'.','9','8','.','.','.','.','6','.'}, 
+                new char[]{'8','.','.','.','6','.','.','.','3'}, 
+                new char[]{'4','.','.','8','.','3','.','.','1'}, 
+                new char[]{'7','.','.','.','2','.','.','.','6'}, 
+                new char[]{'.','6','.','.','.','.','2','8','.'}, 
+                new char[]{'.','.','.','4','1','9','.','.','5'}, 
+                new char[]{'.','.','.','.','8','.','.','7','9'}, 
+            };
+            bool expected = false;
+            Assert.AreEqual(expected,new ValidSudoku().IsValidSudoku(input));
+        }
+        [Test()]
+        public void IsValidSudokuTest2()
+        {
+            char[][] input=new char[][]
+            {
+                new char[]{'5','3','.','.','7','.','.','.','.'}, 
+                new char[]{'6','5','.','1','9','5','.','.','.'}, 
+                new char[]{'.','9','8','.','.','.','.','6','.'}, 
+                new char[]{'8','.','.','.','6','.','.','.','3'}, 
+                new char[]{'4','.','.','8','.','3','.','.','1'}, 
+                new char[]{'7','.','.','.','2','.','.','.','6'}, 
+                new char[]{'.','6','.','.','.','.','2','8','.'}, 
+                new char[]{'.','.','.','4','1','9','.','.','5'}, 
+                new char[]{'.','.','.','.','8','.','.','7','9'}, 
+            };
+            bool expected = false;
+            Assert.AreEqual(expected,new ValidSudoku().IsValidSudoku(input));
+        }
+    }
+}
---
+++ src/LeetCode/HashTable/ValidSudoku.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace LeetCode.HashTable
+{
+    /// <summary>
+    /// 36. 有效的数独
+    /// </summary>
+   public class ValidSudoku
+    {
+        public bool IsValidSudoku(char[][] board) 
+        {
+            Dictionary<char,List<Tuple<int,int>>> numIndexes=new Dictionary<char, List<Tuple<int,int>>>(board.Length);
+            Dictionary<int,List<char>> areaNums= new Dictionary<int,List<char>>(board.Length);
+            for (int i = 0; i < board.Length; i++)
+            {
+                for (int j = 0; j < board[i].Length; j++)
+                {
+                    char tempChar = board[i][j];
+                    if (tempChar == '.')
+                    {
+                        continue;
+                    }
+                    if (numIndexes.ContainsKey(tempChar))
+                    {
+                        var tempList = numIndexes[tempChar];
+                        if (tempList.FirstOrDefault(u => u.Item1 == i || u.Item2 == j) != null)
+                        {
+                            return false;
+                        }
+                        tempList.Add(new Tuple<int, int>(i,j));
+                    }
+                    else
+                    {
+                        numIndexes.Add(tempChar,new List<Tuple<int, int>>(){new Tuple<int, int>(i,j)});
+                    }
+
+                    int areaNo = this.GetAreaNo(i, j);
+                    if (areaNums.ContainsKey(areaNo))
+                    {
+                        var tempList = areaNums[areaNo];
+                        if (tempList.Contains(tempChar))
+                        {
+                            return false;
+                        }
+                        tempList.Add(tempChar);
+                    }
+                    else
+                    {
+                        areaNums.Add(areaNo,new List<char>(){tempChar});
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        private int GetAreaNo(int rowIndex, int columnIndex)
+        {
+            return (rowIndex / 3) * 3 + columnIndex / 3;
+        }
+    }
+}
