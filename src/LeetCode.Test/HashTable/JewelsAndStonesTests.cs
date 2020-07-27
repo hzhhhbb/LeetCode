1ec2c56bb2d3b6646ce4ecae5c065aa80c327d4f
Vincent <huangzihengengineer@gmail.com>
2020-07-27 20:44:15 +0800

    771. 宝石与石头

---
+++ src/LeetCode.Test/HashTable/JewelsAndStonesTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using LeetCode.HashTable;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Tests.HashTable
+{
+    [TestFixture()]
+    public class JewelsAndStonesTests
+    {
+        [Test()]
+        public void NumJewelsInStonesTest()
+        {
+            string J = "aA";
+            string S = "aAAbbbb";
+            int expected = 3;
+            Assert.AreEqual(expected,new JewelsAndStones().NumJewelsInStones(J,S));
+        }
+
+        [Test()]
+        public void NumJewelsInStonesTest1()
+        {
+            string J = "z";
+            string S = "ZZ";
+            int expected = 0;
+            Assert.AreEqual(expected,new JewelsAndStones().NumJewelsInStones(J,S));
+        }
+
+        [Test()]
+        public void NumJewelsInStonesTest2()
+        {
+            string J = "";
+            string S = "ZZ";
+            int expected = 0;
+            Assert.AreEqual(expected,new JewelsAndStones().NumJewelsInStones(J,S));
+        }
+
+        [Test()]
+        public void NumJewelsInStonesTest3()
+        {
+            string J = "a";
+            string S = "";
+            int expected = 0;
+            Assert.AreEqual(expected,new JewelsAndStones().NumJewelsInStones(J,S));
+        }
+    }
+}
---
+++ src/LeetCode/HashTable/JewelsAndStones.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.HashTable
+{
+    /// <summary>
+    /// 771. 宝石与石头
+    /// </summary>
+   public class JewelsAndStones
+    {
+        public int NumJewelsInStones(string J, string S) 
+        {
+            HashSet<char> jewels=new HashSet<char>(J.ToCharArray());
+            int count = 0;
+            foreach (char s in S)
+            {
+                if (jewels.Contains(s))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
