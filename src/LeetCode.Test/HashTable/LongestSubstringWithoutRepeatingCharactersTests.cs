797bcfb54420857adfdaf38e40b081d426ffcf14
Vincent <huangzihengengineer@gmail.com>
2020-07-27 21:49:27 +0800

    3. 无重复字符的最长子串

---
+++ src/LeetCode.Test/HashTable/LongestSubstringWithoutRepeatingCharactersTests.cs
@@ -0,0 +1,89 @@
+﻿using NUnit.Framework;
+using LeetCode.HashTable;
+
+namespace LeetCode.Tests.HashTable
+{
+    [TestFixture()]
+    public class LongestSubstringWithoutRepeatingCharactersTests
+    {
+        [Test()]
+        public void LengthOfLongestSubstringTest()
+        {
+            string input = "abcabcbb";
+            int expected = 3;
+            Assert.AreEqual(expected,new LongestSubstringWithoutRepeatingCharacters().LengthOfLongestSubstring(input));
+        }
+
+        [Test()]
+        public void LengthOfLongestSubstringTest1()
+        {
+            string input = "12345678901abcdefghijk";
+            int expected = 21;
+            Assert.AreEqual(expected,new LongestSubstringWithoutRepeatingCharacters().LengthOfLongestSubstring(input));
+        }
+
+        [Test()]
+        public void LengthOfLongestSubstringTest2()
+        {
+            string input = "12345678901abcd1efghijk";
+            int expected = 14;
+            Assert.AreEqual(expected,new LongestSubstringWithoutRepeatingCharacters().LengthOfLongestSubstring(input));
+        }
+
+        [Test()]
+        public void LengthOfLongestSubstringTest3()
+        {
+            string input = "";
+            int expected = 0;
+            Assert.AreEqual(expected,new LongestSubstringWithoutRepeatingCharacters().LengthOfLongestSubstring(input));
+        }
+
+        [Test()]
+        public void LengthOfLongestSubstringTest4()
+        {
+            string input = "1234567890zxcvbnmasdfghjkl";
+            int expected = 26;
+            Assert.AreEqual(expected,new LongestSubstringWithoutRepeatingCharacters().LengthOfLongestSubstring(input));
+        }
+
+        [Test()]
+        public void LengthOfLongestSubstringTest5()
+        {
+            string input = "abba";
+            int expected = 2;
+            Assert.AreEqual(expected,new LongestSubstringWithoutRepeatingCharacters().LengthOfLongestSubstring(input));
+        }
+
+        [Test()]
+        public void LengthOfLongestSubstringTest6()
+        {
+            string input = "ababcabababababababab";
+            int expected = 3;
+            Assert.AreEqual(expected,new LongestSubstringWithoutRepeatingCharacters().LengthOfLongestSubstring(input));
+        }
+
+        [Test()]
+        public void LengthOfLongestSubstringTest7()
+        {
+            string input = "aaaaaaaaaaaaaaa";
+            int expected = 1;
+            Assert.AreEqual(expected,new LongestSubstringWithoutRepeatingCharacters().LengthOfLongestSubstring(input));
+        }
+
+        [Test()]
+        public void LengthOfLongestSubstringTest8()
+        {
+            string input = "abcaaaaaaaaaaaaaaabbbbbbbbbbabcde";
+            int expected = 5;
+            Assert.AreEqual(expected,new LongestSubstringWithoutRepeatingCharacters().LengthOfLongestSubstring(input));
+        }
+
+        [Test()]
+        public void LengthOfLongestSubstringTest9()
+        {
+            string input = "abcdeaaaaaaaaabbbbabababababaababababababb";
+            int expected = 5;
+            Assert.AreEqual(expected,new LongestSubstringWithoutRepeatingCharacters().LengthOfLongestSubstring(input));
+        }
+    }
+}
---
+++ src/LeetCode/HashTable/longestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.HashTable
+{
+    /// <summary>
+    /// 3. 无重复字符的最长子串
+    /// </summary>
+    public class LongestSubstringWithoutRepeatingCharacters
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (s.Length == 0)
+            {
+                return s.Length;
+            }
+
+            int tempLongestLength = 0;
+            int longestLength = 0;
+            Dictionary<char, int> charLastIndexes = new Dictionary<char, int>();
+            for (var index = 0; index < s.Length; index++)
+            {
+                char c = s[index];
+                if (charLastIndexes.ContainsKey(c))
+                {
+                    longestLength = Math.Max(tempLongestLength, longestLength);
+                    tempLongestLength = Math.Min(index - charLastIndexes[c], tempLongestLength + 1);
+                    charLastIndexes[c] = index;
+                }
+                else
+                {
+                    charLastIndexes.Add(c, index);
+                    tempLongestLength++;
+                }
+            }
+
+            longestLength = Math.Max(tempLongestLength, longestLength);
+            return longestLength;
+        }
+    }
+}
+/* 方法一：
+ * 用字典记录重复字符的最近位置，算出下一次迭代的初始长度。如果初始长度大于已有的长度，说明字符中间有重叠的字符，类似“caac”这样的，那么初始长度+1即可
+ *
+ * 方法二：队列
+ * 迭代字符串，遇到重复字符，把队头出队，记录每次的长度，返回最大长度即可
+ */
