6eea46808ee4447f70d46b945ed21202aa04baab
Vincent <huangzihengengineer@gmail.com>
2020-07-28 23:43:18 +0800

    347. 前 K 个高频元素

---
+++ src/LeetCode.Test/HashTable/TopKFrequentElementsTests.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+using LeetCode.HashTable;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Tests.HashTable
+{
+    [TestFixture()]
+    public class TopKFrequentElementsTests
+    {
+        [Test()]
+        public void TopKFrequentTest()
+        {
+            int[] nums = {1, 1, 1, 2, 2, 3};
+            int k = 2;
+            int[] expected = {1, 2};
+            for (int i = 0; i < expected.Length; i++)
+            {
+                var result=new TopKFrequentElements().TopKFrequent(nums,k);
+                if (!result.Contains(expected[i]))
+                {
+                    Assert.Fail();
+                }
+            }
+
+            Assert.Pass();
+        }
+
+        [Test()]
+        public void TopKFrequentTest1()
+        {
+            int[] nums = {1};
+            int k = 1;
+            int[] expected = {1};
+            for (int i = 0; i < expected.Length; i++)
+            {
+                var result=new TopKFrequentElements().TopKFrequent(nums,k);
+                if (!result.Contains(expected[i]))
+                {
+                    Assert.Fail();
+                }
+            }
+
+            Assert.Pass();
+        }
+
+        [Test()]
+        public void TopKFrequentTest2()
+        {
+            int[] nums = {1,2};
+            int k = 2;
+            int[] expected = {1,2};
+            for (int i = 0; i < expected.Length; i++)
+            {
+                var result=new TopKFrequentElements().TopKFrequent(nums,k);
+                if (!result.Contains(expected[i]))
+                {
+                    Assert.Fail();
+                }
+            }
+
+            Assert.Pass();
+        }
+    }
+}
---
+++ src/LeetCode/HashTable/TopKFrequentElements.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.HashTable
+{
+    /// <summary>
+    /// 347. 前 K 个高频元素
+    /// </summary>
+    public class TopKFrequentElements
+    {
+        public int[] TopKFrequent(int[] nums, int k)
+        {
+            Dictionary<int, int> numberAndCount = new Dictionary<int, int>();
+            
+            foreach (int num in nums)
+            {
+                if (numberAndCount.ContainsKey(num))
+                {
+                    numberAndCount[num]++;
+                   
+                }
+                else
+                {
+                    numberAndCount.Add(num, 1);
+                }
+            }
+
+            return numberAndCount.OrderByDescending(u=>u.Value).Select(u=>u.Key).Take(k).ToArray();
+        }
+    }
+}
