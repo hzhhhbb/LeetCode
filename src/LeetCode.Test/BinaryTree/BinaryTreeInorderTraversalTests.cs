e371d696243faa314e3d5462978e84e9643016c5
Vincent <huangzihengengineer@gmail.com>
2020-08-01 02:10:09 +0800

    94. 二叉树的中序遍历

---
+++ src/LeetCode.Test/BinaryTree/BinaryTreeInorderTraversalTests.cs
@@ -0,0 +1,107 @@
+﻿using NUnit.Framework;
+using LeetCode.BinaryTree;
+using System.Collections.Generic;
+
+namespace LeetCode.Tests.BinaryTree
+{
+    [TestFixture()]
+    [Timeout(10000)]
+    public class BinaryTreeInorderTraversalTests
+    {
+         [Test()]
+        public void InorderTraversalTest()
+        {
+            TreeNode root=new TreeNode(1);
+            root.right = new TreeNode(2);
+            root.right.left=new TreeNode(3);
+            
+            IList<int> expected=new List<int>(){1,3,2};
+            var result = new BinaryTreeInorderTraversal().InorderTraversal(root);
+            Assert.AreEqual(expected,result);
+        }
+
+        [Test()]
+        public void InorderTraversalTest1()
+        {
+            TreeNode root=new TreeNode(1);
+            root.left=new TreeNode(2);
+            root.left.left=new TreeNode(4);
+            root.left.right=new TreeNode(5);
+            root.right=new TreeNode(3);
+            root.right.left=new TreeNode(6);
+            root.right.right=new TreeNode(7);
+            
+            IList<int> expected=new List<int>(){4,2,5,1,6,3,7};
+            var result = new BinaryTreeInorderTraversal().InorderTraversal(root);
+            Assert.AreEqual(expected,result);
+        }
+
+        [Test()]
+        public void InorderTraversalTest2()
+        {
+            TreeNode root=new TreeNode(1);
+            
+            IList<int> expected=new List<int>(){1};
+            var result = new BinaryTreeInorderTraversal().InorderTraversal(root);
+            Assert.AreEqual(expected,result);
+        }
+
+        [Test()]
+        public void InorderTraversalTest3()
+        {
+            TreeNode root = null;
+            
+            IList<int> expected=new List<int>(){};
+            var result = new BinaryTreeInorderTraversal().InorderTraversal(root);
+            Assert.AreEqual(expected,result);
+        }
+
+        [Test()]
+        public void InorderTraversalOfIeterationTest()
+        {
+            TreeNode root=new TreeNode(1);
+            root.right = new TreeNode(2);
+            root.right.left=new TreeNode(3);
+            
+            IList<int> expected=new List<int>(){1,3,2};
+            var result = new BinaryTreeInorderTraversal().InorderTraversalOfIteration(root);
+            Assert.AreEqual(expected,result);
+        }
+
+        [Test()]
+        public void InorderTraversalOfIeterationTest1()
+        {
+            TreeNode root=new TreeNode(1);
+            root.left=new TreeNode(2);
+            root.left.left=new TreeNode(4);
+            root.left.right=new TreeNode(5);
+            root.right=new TreeNode(3);
+            root.right.left=new TreeNode(6);
+            root.right.right=new TreeNode(7);
+            
+            IList<int> expected=new List<int>(){4,2,5,1,6,3,7};
+            var result = new BinaryTreeInorderTraversal().InorderTraversalOfIteration(root);
+            Assert.AreEqual(expected,result);
+        }
+
+        [Test()]
+        public void InorderTraversalOfIeterationTest2()
+        {
+            TreeNode root=new TreeNode(1);
+            
+            IList<int> expected=new List<int>(){1};
+            var result = new BinaryTreeInorderTraversal().InorderTraversalOfIteration(root);
+            Assert.AreEqual(expected,result);
+        }
+
+        [Test()]
+        public void InorderTraversalOfIeterationTest3()
+        {
+            TreeNode root = null;
+            
+            IList<int> expected=new List<int>(){};
+            var result = new BinaryTreeInorderTraversal().InorderTraversalOfIteration(root);
+            Assert.AreEqual(expected,result);
+        }
+    }
+}
---
+++ src/LeetCode/BinaryTree/BinaryTreeInorderTraversal.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.BinaryTree
+{
+    /// <summary>
+    /// 94. 二叉树的中序遍历
+    /// </summary>
+    public class BinaryTreeInorderTraversal
+    {
+        //中序遍历顺序：左节点、根节点、右节点
+
+        // 递归算法
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            var result = new List<int>();
+
+            if (root != null)
+            {
+                this.Inorder(root, ref result);
+            }
+
+            return result;
+        }
+
+        private void Inorder(TreeNode root, ref List<int> result)
+        {
+            if (root.left != null)
+            {
+                this.Inorder(root.left, ref result);
+            }
+
+            result.Add(root.val);
+
+            if (root.right != null)
+            {
+                this.Inorder(root.right, ref result);
+            }
+        }
+
+
+        // 迭代算法
+        public IList<int> InorderTraversalOfIteration(TreeNode root)
+        {
+            List<int> result=new List<int>();
+            if (root == null)
+            {
+                return result;
+            }
+
+            Stack<TreeNode> stack=new Stack<TreeNode>();
+            TreeNode curr = root;
+            while (stack.Any()||curr!=null)
+            {
+                while (curr!=null)
+                {
+                   stack.Push(curr);
+                   curr = curr.left;
+                }
+
+                curr = stack.Pop();
+                result.Add(curr.val);
+                curr = curr.right;
+
+            }
+
+            return result;
+        }
+    }
+}
