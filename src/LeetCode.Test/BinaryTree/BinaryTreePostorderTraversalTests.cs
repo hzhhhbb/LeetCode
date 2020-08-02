ed0a51422a1f472a0dab50dfbf5ca0b408c841d4
Vincent <huangzihengengineer@gmail.com>
2020-08-02 21:50:11 +0800

    145. 二叉树的后序遍历

---
+++ src/LeetCode.Test/BinaryTree/BinaryTreePostorderTraversalTests.cs
@@ -0,0 +1,108 @@
+﻿using NUnit.Framework;
+using LeetCode.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Tests.BinaryTree
+{
+    [TestFixture()]
+    public class BinaryTreePostorderTraversalTests
+    {
+        [Test()]
+        public void PostorderTraversalTest()
+        {
+            TreeNode root = new TreeNode(1);
+            root.right = new TreeNode(2);
+            root.right.left = new TreeNode(3);
+
+            IList<int> expected = new List<int>() {3, 2, 1};
+            var result = new BinaryTreePostorderTraversal().PostorderTraversal(root);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test()]
+        public void PostorderTraversalTest1()
+        {
+            TreeNode root = new TreeNode(1);
+            root.left = new TreeNode(2);
+            root.left.left = new TreeNode(4);
+            root.left.right = new TreeNode(5);
+            root.right = new TreeNode(3);
+            root.right.left = new TreeNode(6);
+            root.right.right = new TreeNode(7);
+
+            IList<int> expected = new List<int>() {4, 5, 2, 6, 7, 3, 1};
+            var result = new BinaryTreePostorderTraversal().PostorderTraversal(root);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test()]
+        public void PostorderTraversalTest2()
+        {
+            TreeNode root = new TreeNode(1);
+
+            IList<int> expected = new List<int>() {1};
+            var result = new BinaryTreePostorderTraversal().PostorderTraversal(root);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test()]
+        public void PostorderTraversalTest3()
+        {
+            TreeNode root = null;
+
+            IList<int> expected = new List<int>() { };
+            var result = new BinaryTreePostorderTraversal().PostorderTraversal(root);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test()]
+        public void PostorderTraversalOfIeterationTest()
+        {
+            TreeNode root = new TreeNode(1);
+            root.right = new TreeNode(2);
+            root.right.left = new TreeNode(3);
+
+            IList<int> expected = new List<int>() {3,2,1};
+            var result = new BinaryTreePostorderTraversal().PostorderTraversalOfIteration(root);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test()]
+        public void PostorderTraversalOfIeterationTest1()
+        {
+            TreeNode root = new TreeNode(1);
+            root.left = new TreeNode(2);
+            root.left.left = new TreeNode(4);
+            root.left.right = new TreeNode(5);
+            root.right = new TreeNode(3);
+            root.right.left = new TreeNode(6);
+            root.right.right = new TreeNode(7);
+
+            IList<int> expected = new List<int>() {4, 5, 2, 6, 7, 3, 1};
+            var result = new BinaryTreePostorderTraversal().PostorderTraversalOfIteration(root);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test()]
+        public void PostorderTraversalOfIeterationTest2()
+        {
+            TreeNode root = new TreeNode(1);
+
+            IList<int> expected = new List<int>() {1};
+            var result = new BinaryTreePostorderTraversal().PostorderTraversalOfIteration(root);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test()]
+        public void PostorderTraversalOfIeterationTest3()
+        {
+            TreeNode root = null;
+
+            IList<int> expected = new List<int>() { };
+            var result = new BinaryTreePostorderTraversal().PostorderTraversalOfIteration(root);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ src/LeetCode/BinaryTree/BinaryTreePostorderTraversal.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.BinaryTree
+{
+    public class BinaryTreePostorderTraversal
+    {
+        //中序遍历顺序：左节点、根节点、右节点
+
+        // 递归解法
+        public IList<int> PostorderTraversal(TreeNode root)
+        {
+            var result = new List<int>();
+
+            if (root != null)
+            {
+                this.Postorder(root, ref result);
+            }
+
+            return result;
+        }
+
+        private void Postorder(TreeNode root, ref List<int> result)
+        {
+            if (root.left != null)
+            {
+                this.Postorder(root.left, ref result);
+            }
+
+            if (root.right != null)
+            {
+                this.Postorder(root.right, ref result);
+            }
+
+            result.Add(root.val);
+        }
+
+        // 迭代解法
+        public IList<int> PostorderTraversalOfIteration(TreeNode root)
+        {
+            List<int> result = new List<int>();
+            if (root == null)
+            {
+                return result;
+            }
+
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            stack.Push(root);
+            while (stack.Any())
+            {
+                TreeNode node = stack.Pop();
+                result.Add(node.val);
+
+                if (node.left != null)
+                {
+                    stack.Push(node.left);
+                }
+
+                if (node.right != null)
+                {
+                    stack.Push(node.right);
+                }
+            }
+            
+            result.Reverse();
+            return result;
+        }
+    }
+}
+/*
+ * 解题思路
+ * 前序遍历顺序：根左右
+ * 中序遍历顺序：左根右
+ * 后序遍历顺序：左右根，反过来就是根右左
+ * 调整一下前序遍历的顺序即可
+ */
