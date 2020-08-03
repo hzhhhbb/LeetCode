df127073237edc2095ff26be4479a1862accf1cf
vincent <huangzihengengineer@gmail.com>
2020-08-03 17:53:46 +0800

    104. 二叉树的最大深度

---
+++ src/LeetCode.Test/BinaryTree/MaximumDepthOfBinaryTreeTests.cs
@@ -0,0 +1,92 @@
+﻿using NUnit.Framework;
+using LeetCode.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Tests.BinaryTree
+{
+    [TestFixture()]
+    public class MaximumDepthOfBinaryTreeTests
+    {
+        [Test()]
+        public void MaxDepthTest()
+        {
+            TreeNode root = new TreeNode(1);
+            root.left = new TreeNode(2);
+            root.left.left = new TreeNode(4);
+            root.left.right = new TreeNode(5);
+            root.right = new TreeNode(3);
+            root.right.left = new TreeNode(6);
+            root.right.right = new TreeNode(7);
+
+            int expected = 3;
+            var maxDepth=new MaximumDepthOfBinaryTree().MaxDepth(root);
+
+            Assert.AreEqual(expected,maxDepth);
+        }
+
+        [Test()]
+        public void MaxDepthTest1()
+        {
+            TreeNode root = null;
+
+            int expected = 0;
+            var maxDepth = new MaximumDepthOfBinaryTree().MaxDepth(root);
+
+            Assert.AreEqual(expected, maxDepth);
+        }
+
+        [Test()]
+        public void MaxDepthTest2()
+        {
+            TreeNode root = new TreeNode(1);
+
+            int expected = 1;
+            var maxDepth = new MaximumDepthOfBinaryTree().MaxDepth(root);
+
+            Assert.AreEqual(expected, maxDepth);
+        }
+
+        [Test()]
+        public void MaxDepthTest3()
+        {
+            TreeNode root = new TreeNode(1);
+            root.left=new TreeNode(1);
+            root.left.left = new TreeNode(1);
+            root.left.left.left = new TreeNode(1);
+            root.left.left.left.left = new TreeNode(1);
+            root.left.left.left.left.left = new TreeNode(1);
+            root.left.left.left.left.left.left = new TreeNode(1);
+            int expected = 7;
+            var maxDepth = new MaximumDepthOfBinaryTree().MaxDepth(root);
+
+            Assert.AreEqual(expected, maxDepth);
+        }
+
+        [Test()]
+        public void MaxDepthTest4()
+        {
+            TreeNode root = new TreeNode(1);
+            root.left = new TreeNode(1);
+            root.left.left = new TreeNode(1);
+            root.left.left.left = new TreeNode(1);
+            root.left.left.left.left = new TreeNode(1);
+            root.left.left.left.left.left = new TreeNode(1);
+            root.left.left.left.left.left.left = new TreeNode(1);
+
+            root.right = new TreeNode(1);
+            root.right.right = new TreeNode(1);
+            root.right.right.right = new TreeNode(1);
+            root.right.right.right.right = new TreeNode(1);
+            root.right.right.right.right.right = new TreeNode(1);
+            root.right.right.right.right.right.right = new TreeNode(1);
+            root.right.right.right.right.right.right.right = new TreeNode(1);
+
+            int expected = 8;
+            var maxDepth = new MaximumDepthOfBinaryTree().MaxDepth(root);
+
+            Assert.AreEqual(expected, maxDepth);
+        }
+    }
+}
---
+++ src/LeetCode/BinaryTree/MaximumDepthOfBinaryTree.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LeetCode.BinaryTree
+{
+    /// <summary>
+    /// 104. 二叉树的最大深度
+    /// </summary>
+    public class MaximumDepthOfBinaryTree
+    {
+        private int maxDepth = 0;
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            this.GetMaxDepthOfBinaryTree(root,1);
+            return maxDepth;
+        }
+
+        private void GetMaxDepthOfBinaryTree(TreeNode node,int depth)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            if (node.left == null && node.right == null)
+            {
+                this.maxDepth = Math.Max(this.maxDepth, depth);
+            }
+
+            this.GetMaxDepthOfBinaryTree(node.left, depth + 1);
+            this.GetMaxDepthOfBinaryTree(node.right, depth + 1);
+        }
+    }
+}
