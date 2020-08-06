6bc15bb69553be41987f92c8f9154bd12ba8f7cd
Vincent <huangzihengengineer@gmail.com>
2020-08-06 22:17:16 +0800

    105. 从前序与中序遍历序列构造二叉树

---
+++ src/LeetCode.Test/BinaryTree/ConstructBinaryTreeFromInorderAndPostorderTraversalTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using LeetCode.BinaryTree;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Tests.BinaryTree
+{
+    [TestFixture()]
+    public class ConstructBinaryTreeFromInorderAndPostorderTraversalTests
+    {
+        [Test()]
+        public void BuildTreeTest()
+        {
+            int[] postorder = {9,15,7,20,3};
+            int[] inorder = {9,3,15,20,7};
+            
+            var tree=new ConstructBinaryTreeFromInorderAndPostorderTraversal().BuildTree(inorder,postorder);
+
+            Assert.AreEqual(postorder,new BinaryTreePostorderTraversal().PostorderTraversal(tree).ToArray());
+            Assert.AreEqual(inorder,new BinaryTreeInorderTraversal().InorderTraversal(tree).ToArray());
+        }
+    }
+}
---
+++ src/LeetCode/BinaryTree/ConstructBinaryTreeFromInorderAndPostorderTraversal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.BinaryTree
+{
+    /// <summary>
+    /// 106. 从中序与后序遍历序列构造二叉树
+    /// </summary>
+   public class ConstructBinaryTreeFromInorderAndPostorderTraversal
+    {
+        private Dictionary<int, int> inorderIndexes;
+        public TreeNode BuildTree(int[] inorder, int[] postorder) {
+            if (inorder.Length == 0)
+            {
+                return null;
+            }
+
+            int nodeCount = inorder.Length;
+            inorderIndexes=new Dictionary<int, int>(nodeCount);
+
+            for (int i = 0; i < inorder.Length; i++)
+            {
+                inorderIndexes.Add(inorder[i],i);
+            }
+
+            return this.BuildTree(postorder, 0, nodeCount - 1, 0, nodeCount - 1);
+        }
+
+
+        private TreeNode BuildTree(int[] postorder, int postorderLeftIndex, int postorderRightIndex,
+            int inorderLeftIndex, int inorderRightIndex)
+        {
+            if (postorderRightIndex < postorderLeftIndex)
+            {
+                return null;
+            }
+
+            int postorderRootIndex = postorderRightIndex;
+
+            int inorderRootIndex = inorderIndexes[postorder[postorderRootIndex]];
+
+            //右子树的节点数目
+            int sizeOfRightSubtree = inorderRightIndex - inorderRootIndex;
+
+            TreeNode root=new TreeNode(postorder[postorderRootIndex]);
+
+            root.right = BuildTree(postorder, postorderRootIndex -sizeOfRightSubtree, postorderRootIndex - 1, inorderRootIndex + 1,
+                inorderRightIndex);
+
+            root.left = BuildTree(postorder,postorderLeftIndex, postorderRootIndex-1-sizeOfRightSubtree, inorderLeftIndex, inorderRootIndex-1);
+
+            return root;
+        }
+    }
+}
