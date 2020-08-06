2a317ebd8e4b9a9f900caeefbfc29626aa26824f
Vincent <huangzihengengineer@gmail.com>
2020-08-06 23:24:32 +0800

    116. 填充每个节点的下一个右侧节点指针

---
+++ src/LeetCode.Test/BinaryTree/PopulatingNextRightPointersInEachNodeTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using LeetCode.BinaryTree;
+
+namespace LeetCode.Tests.BinaryTree
+{
+    [TestFixture()]
+    public class PopulatingNextRightPointersInEachNodeTests
+    {
+        [Test()]
+        public void ConnectTest()
+        {
+            Node root=new Node(1);
+            root.left=new Node(2);
+            root.left.left=new Node(4);
+            root.left.right=new Node(5);
+            root.right=new Node(3);
+            root.right.left=new Node(6);
+            root.right.right=new Node(7);
+            new PopulatingNextRightPointersInEachNode().Connect(root);
+
+            Assert.IsTrue(root.left.next.val==3);
+            Assert.IsTrue(root.left.left.next.val==5);
+            Assert.IsTrue(root.left.right.next.val==6);
+            Assert.IsTrue(root.right.left.next.val==7);
+        }
+    }
+}
---
+++ src/LeetCode/BinaryTree/PopulatingNextRightPointersInEachNode.cs
@@ -0,0 +1,59 @@
+﻿namespace LeetCode.BinaryTree
+{
+    /// <summary>
+    /// 116. 填充每个节点的下一个右侧节点指针
+    /// </summary>
+    public class PopulatingNextRightPointersInEachNode
+    {
+        public Node Connect(Node root)
+        {
+            if (root == null)
+            {
+                return root;
+            }
+
+            this.Connect(root.left, root.right);
+            return root;
+        }
+
+        private void Connect(Node left, Node right)
+        {
+            if (left != null)
+            {
+                left.next = right;
+                this.Connect(left.left, left.right);
+                this.Connect(left.right, right.left);
+            }
+
+            if (right != null)
+            {
+                this.Connect(right.left, right.right);
+            }
+        }
+    }
+
+    public class Node
+    {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node()
+        {
+        }
+
+        public Node(int _val)
+        {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next)
+        {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+}
