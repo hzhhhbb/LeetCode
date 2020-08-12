4b8cb8b2102e904927553aab36a6b66654890526
Vincent <huangzihengengineer@gmail.com>
2020-08-13 00:19:19 +0800

    117. 填充每个节点的下一个右侧节点指针 II

---
+++ src/LeetCode.Test/BinaryTree/PopulatingNextRightPointersInEachNodeIITests.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using LeetCode.BinaryTree;
+
+namespace LeetCode.Tests.BinaryTree
+{
+    [TestFixture()]
+    public class PopulatingNextRightPointersInEachNodeIITests
+    {
+        [Test()]
+        public void ConnectTest()
+        {
+            Node root=new Node(1);
+            root.left=new Node(2);
+            root.left.left=new Node(4);
+            root.left.right=new Node(5);
+            root.right=new Node(3);
+           
+            root.right.right=new Node(7);
+            new PopulatingNextRightPointersInEachNodeII().Connect(root);
+
+            Assert.IsTrue(root.left.next.val==3);
+            Assert.IsTrue(root.left.left.next.val==5);
+            Assert.IsTrue(root.left.right.next.val==7);
+        }
+
+        [Test()]
+        public void ConnectTest1()
+        {
+            Node root=new Node(1);
+            root.left=new Node(2);
+            root.left.left=new Node(4);
+            root.right=new Node(3);
+            root.right.right=new Node(5);
+            new PopulatingNextRightPointersInEachNodeII().Connect(root);
+
+            Assert.IsTrue(root.left.next.val==3);
+            Assert.IsTrue(root.left.left.next.val==5);
+        }
+
+        [Test()]
+        public void ConnectTest2()
+        {
+            Node root=new Node(1);
+            root.left=new Node(2);
+            root.left.left=new Node(3);
+            root.left.left.left=new Node(5);
+            new PopulatingNextRightPointersInEachNodeII().Connect(root);
+
+           Assert.IsTrue(root.left.next==null);
+        }
+        [Test()]
+        public void ConnectTest3()
+        {
+            Node root=new Node(1);
+            root.left=new Node(2);
+            root.left.left=new Node(4);
+            root.left.right=new Node(5);
+            root.left.left.left=new Node(7);
+            root.right=new Node(3);
+            root.right.right=new Node(6);
+            root.right.right.right=new Node(8);
+            new PopulatingNextRightPointersInEachNodeII().Connect(root);
+
+            Assert.IsTrue( root.left.left.left.next.val==8);
+        }
+    }
+}
---
+++ src/LeetCode/BinaryTree/PopulatingNextRightPointersInEachNodeII.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.BinaryTree
+{
+    /// <summary>
+    /// 117. 填充每个节点的下一个右侧节点指针 II
+    /// </summary>
+    public class PopulatingNextRightPointersInEachNodeII
+    {
+        public Node Connect(Node root)
+        {
+            //空间、时间复杂度O(n)
+
+            if (root == null)
+            {
+                return root;
+            }
+
+            // Initialize a queue data structure which contains
+            // just the root of the tree
+            Queue<Node> Q = new Queue<Node>();
+            Q.Enqueue(root);
+
+            // Outer while loop which iterates over 
+            // each level
+            while (Q.Any())
+            {
+                // Note the size of the queue
+                int size = Q.Count;
+
+                // Iterate over all the nodes on the current level
+                for (int i = 0; i < size; i++)
+                {
+                    // Pop a node from the front of the queue
+                    Node node = Q.Dequeue();
+
+                    // This check is important. We don't want to
+                    // establish any wrong connections. The queue will
+                    // contain nodes from 2 levels at most at any
+                    // point in time. This check ensures we only 
+                    // don't establish next pointers beyond the end
+                    // of a level
+                    if (i < size - 1)
+                    {
+                        node.next = Q.Peek();
+                    }
+
+                    // Add the children, if any, to the back of
+                    // the queue
+                    if (node.left != null)
+                    {
+                        Q.Enqueue(node.left);
+                    }
+
+                    if (node.right != null)
+                    {
+                        Q.Enqueue(node.right);
+                    }
+                }
+            }
+
+            // Since the tree has now been modified, return the root node
+            return root;
+        }
+    }
+}
