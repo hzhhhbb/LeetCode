19c1ef23a068e4dc1ce012c50152ecda0fb73108
Vincent <huangzihengengineer@gmail.com>
2020-08-15 01:19:03 +0800

    98. 验证二叉搜索树

---
+++ src/LeetCode.Test/BinarySearchTree/ValidateBinarySearchTreeTests.cs
@@ -0,0 +1,92 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode.Tests.BinarySearchTree
+{
+    using LeetCode.BinarySearchTree;
+    using LeetCode.BinaryTree;
+
+    [TestFixture()]
+    public class ValidateBinarySearchTreeTests
+    {
+        [Test()]
+        public void IsValidBSTTest()
+        {
+            TreeNode root=new TreeNode(1);
+            root.left=new TreeNode(2);
+            root.right=new TreeNode(3);
+            bool expected = false;
+
+            bool result=new ValidateBinarySearchTree().IsValidBST(root);
+
+            Assert.AreEqual(expected,result);
+        }
+
+        [Test()]
+        public void IsValidBSTTest1()
+        {
+            TreeNode root=new TreeNode(2);
+            root.left=new TreeNode(1);
+            root.right=new TreeNode(3);
+            bool expected = true;
+
+            bool result=new ValidateBinarySearchTree().IsValidBST(root);
+
+            Assert.AreEqual(expected,result);
+        }
+
+        [Test()]
+        public void IsValidBSTTest2()
+        {
+            TreeNode root=new TreeNode(5);
+            root.left=new TreeNode(1);
+            root.right=new TreeNode(4);
+            root.right.left=new TreeNode(3);
+            root.right.right=new TreeNode(6);
+            bool expected = false;
+
+            bool result=new ValidateBinarySearchTree().IsValidBST(root);
+
+            Assert.AreEqual(expected,result);
+        }
+
+        [Test()]
+        public void IsValidBSTTest3()
+        {
+            TreeNode root = null;
+            bool expected = true;
+
+            bool result=new ValidateBinarySearchTree().IsValidBST(root);
+
+            Assert.AreEqual(expected,result);
+        }
+
+        [Test()]
+        public void IsValidBSTTest4()
+        {
+            TreeNode root=new TreeNode(1);
+            root.left=new TreeNode(1);
+            bool expected = false;
+
+            bool result=new ValidateBinarySearchTree().IsValidBST(root);
+
+            Assert.AreEqual(expected,result);
+        }
+
+        
+        [Test()]
+        public void IsValidBSTTest5()
+        {
+            //[10,5,15,null,null,6,20]
+            TreeNode root=new TreeNode(10);
+            root.left=new TreeNode(5);
+            root.right=new TreeNode(15);
+            root.right.left=new TreeNode(6);
+            root.right.right=new TreeNode(20);
+            bool expected = false;
+
+            bool result=new ValidateBinarySearchTree().IsValidBST(root);
+
+            Assert.AreEqual(expected,result);
+        }
+    }
+}
---
+++ src/LeetCode/BinarySearchTree/ValidateBinarySearchTree.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.BinarySearchTree
+{
+    using LeetCode.BinaryTree;
+
+    /// <summary>
+    /// 98. 验证二叉搜索树
+    /// </summary>
+    public class ValidateBinarySearchTree
+    {
+        public bool IsValidBST(TreeNode root)
+        {
+            if (root == null)
+            {
+                return true;
+            }
+
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            TreeNode curr = root;
+            long prevValue = -long.MaxValue;
+            while (stack.Any() || curr != null)
+            {
+                while (curr != null)
+                {
+                    stack.Push(curr);
+                    curr = curr.left;
+                }
+
+                curr = stack.Pop();
+
+                //二叉搜索树的中序遍历一定是升序的
+                if (prevValue >= curr.val)
+                {
+                    return false;
+                }
+                else
+                {
+                    prevValue = curr.val;
+                }
+                curr = curr.right;
+            }
+
+            return true;
+        }
+    }
+}
