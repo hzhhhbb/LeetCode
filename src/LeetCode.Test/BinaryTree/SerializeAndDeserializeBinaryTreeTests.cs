24e84c8d0b2e1d98484fb35288bce55524e8b639
vincent <huangzihengengineer@gmail.com>
2020-08-14 11:29:58 +0800

    297. 二叉树的序列化与反序列化

---
+++ src/LeetCode.Test/BinaryTree/SerializeAndDeserializeBinaryTreeTests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using LeetCode.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Tests.BinaryTree
+{
+    [TestFixture()]
+    public class SerializeAndDeserializeBinaryTreeTests
+    {
+
+        [Test()]
+        public void serializeTest()
+        {
+            TreeNode root=new TreeNode(1);
+            root.left=new TreeNode(2);
+            root.right=new TreeNode(3);
+
+            string expected = "1,2,3";
+
+            Assert.AreEqual(expected,new SerializeAndDeserializeBinaryTree().serialize(root));
+        }
+
+        [Test()]
+        public void serializeTest1()
+        {
+            TreeNode root=new TreeNode(1);
+            root.left=new TreeNode(2);
+           root.right=new TreeNode(3);
+           root.right.left=new TreeNode(4);
+           root.right.right=new TreeNode(5);
+
+            string expected = "1,2,3,,,4,5";
+
+            Assert.AreEqual(expected,new SerializeAndDeserializeBinaryTree().serialize(root));
+        }
+
+        [Test()]
+        public void deserializeTest()
+        {
+            string input = "1,2,3,,,4,5";
+
+            TreeNode expected=new TreeNode(1);
+            expected.left=new TreeNode(2);
+            expected.right=new TreeNode(3);
+            expected.right.left=new TreeNode(4);
+            expected.right.right=new TreeNode(5);
+
+            TreeNode result=new SerializeAndDeserializeBinaryTree().deserialize(input);
+
+            Assert.AreEqual(new BinaryTreePreorderTraversal().PreorderTraversal(result),new BinaryTreePreorderTraversal().PreorderTraversal(expected));
+
+            Assert.AreEqual(new BinaryTreeInorderTraversal().InorderTraversal(expected), new BinaryTreeInorderTraversal().InorderTraversal(result));
+
+        }
+
+        [Test()]
+        public void deserializeTest1()
+        {
+            string input = "";
+            TreeNode result=new SerializeAndDeserializeBinaryTree().deserialize(input);
+            Assert.AreEqual(null,result);
+        }
+    }
+}
---
+++ src/LeetCode/BinaryTree/SerializeAndDeserializeBinaryTree.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.BinaryTree
+{
+    using System.Linq;
+
+    /// <summary>
+    /// 297. 二叉树的序列化与反序列化
+    /// </summary>
+    public class SerializeAndDeserializeBinaryTree
+    {
+        // Encodes a tree to a single string.
+        public string serialize(TreeNode root)
+        {
+            if (root == null)
+            {
+                return string.Empty;
+            }
+            List<int?> treeList=new List<int?>();
+            Queue<TreeNode> queue=new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Any())
+            {
+                TreeNode node = queue.Dequeue();
+                if (node == null)
+                {
+                    treeList.Add(null);
+                }
+                else
+                {
+                    treeList.Add(node.val);
+                    queue.Enqueue(node.left);
+                    queue.Enqueue(node.right);
+                }
+            }
+
+            int i = treeList.Count - 1;
+            for (; i >=0; i--)
+            {
+                if (treeList[i] != null)
+                {
+                    break;
+                }
+            }
+            return string.Join(",", treeList.Take(i+1));
+        }
+
+        // Decodes your encoded data to tree.
+        public TreeNode deserialize(string data)
+        {
+            if (string.IsNullOrWhiteSpace(data))
+            {
+                return null;
+            }
+
+            string[] treeNodes = data.Split(",");
+            TreeNode root = null;
+            if (int.TryParse(treeNodes[0], out int rootVal))
+            {
+                root=new TreeNode(rootVal);
+            }
+            else
+            {
+                return root;
+            }
+
+            Queue<TreeNode> queue=new Queue<TreeNode>(treeNodes.Length);
+            queue.Enqueue(root);
+
+            int currentLevelNodeCount = 1;
+            int visitedNodeCount = 1;
+
+            while (queue.Any())
+            {
+                int tempChildNodeCount = 0;
+                for (int i = 0; i < currentLevelNodeCount; i++)
+                {
+                    var node = queue.Dequeue();
+                    if (node != null)
+                    {
+                        if (visitedNodeCount<treeNodes.Length&&int.TryParse(treeNodes[visitedNodeCount], out int leftVal))
+                        {
+                            node.left = new TreeNode(leftVal);
+                            queue.Enqueue(node.left);
+                            tempChildNodeCount++;
+                        }
+
+                        visitedNodeCount++;
+                        if (visitedNodeCount<treeNodes.Length&&int.TryParse(treeNodes[visitedNodeCount], out int rightVal))
+                        {
+                            node.right = new TreeNode(rightVal);
+                            queue.Enqueue(node.right);
+                            tempChildNodeCount++;
+                        }
+                        visitedNodeCount++;
+                    }
+                }
+
+                currentLevelNodeCount = tempChildNodeCount;
+            }
+
+            return root;
+        }
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.deserialize(codec.serialize(root));
