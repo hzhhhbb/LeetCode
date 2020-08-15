86b5087bc94cb4b170d5e8afe334e55fe6a472f8
Vincent <huangzihengengineer@gmail.com>
2020-08-15 16:08:15 +0800

    173. 二叉搜索树迭代器

---
+++ src/LeetCode.Test/BinarySearchTree/SearchInABinarySearchTreeTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using LeetCode.BinarySearchTree;
+using LeetCode.BinaryTree;
+
+namespace LeetCode.Tests.BinarySearchTree
+{
+    [TestFixture()]
+    public class SearchInABinarySearchTreeTests
+    {
+        [Test()]
+        public void SearchBSTTest()
+        {
+            TreeNode root=new TreeNode(4);
+            root.left=new TreeNode(2);
+            root.left.left=new TreeNode(1);
+            root.left.right=new TreeNode(3);
+            root.right=new TreeNode(7);
+
+            int targetValue = 2;
+            TreeNode expected = root.left;
+            TreeNode result = new SearchInABinarySearchTree().SearchBST(root, targetValue);
+            Assert.AreSame(expected,result);
+        }
+
+        
+        [Test()]
+        public void SearchBSTTest1()
+        {
+            TreeNode root = null;
+
+            int targetValue = 2;
+            TreeNode expected = null;
+            TreeNode result = new SearchInABinarySearchTree().SearchBST(root, targetValue);
+            Assert.AreSame(expected,result);
+        }
+
+        [Test()]
+        public void SearchBSTTest2()
+        {
+            TreeNode root=new TreeNode(4);
+            root.left=new TreeNode(2);
+            root.left.left=new TreeNode(1);
+            root.left.right=new TreeNode(3);
+            root.right=new TreeNode(7);
+
+            int targetValue = 8;
+            TreeNode expected = null;
+            TreeNode result = new SearchInABinarySearchTree().SearchBST(root, targetValue);
+            Assert.AreSame(expected,result);
+        }
+    }
+}
---
+++ src/LeetCode/BinarySearchTree/SearchInABinarySearchTree.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.BinaryTree;
+
+namespace LeetCode.BinarySearchTree
+{
+    /// <summary>
+    /// 700. 二叉搜索树中的搜索
+    /// </summary>
+   public class SearchInABinarySearchTree
+    {
+        public TreeNode SearchBST(TreeNode root, int val)
+        {
+            TreeNode currentNode = root;
+            while (currentNode!=null)
+            {
+                if (currentNode.val == val)
+                {
+                    return currentNode;
+                }
+
+                currentNode = val < currentNode.val ? currentNode.left : currentNode.right;
+            }
+
+            return null;
+        }
+    }
+}
