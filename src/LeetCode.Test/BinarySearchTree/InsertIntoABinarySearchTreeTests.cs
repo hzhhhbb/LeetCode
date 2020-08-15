e2bc3112133a33bc782c2aa74563cb39cf61d39c
Vincent <huangzihengengineer@gmail.com>
2020-08-15 17:59:59 +0800

    701. 二叉搜索树中的插入操作

---
+++ src/LeetCode.Test/BinarySearchTree/InsertIntoABinarySearchTreeTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using LeetCode.BinarySearchTree;
+using LeetCode.BinaryTree;
+
+namespace LeetCode.Tests.BinarySearchTree
+{
+    [TestFixture()]
+    public class InsertIntoABinarySearchTreeTests
+    {
+        [Test()]
+        public void InsertIntoBSTTest()
+        {
+            TreeNode root=new TreeNode(4);
+            root.left=new TreeNode(2);
+            root.left.left=new TreeNode(1);
+            root.left.right=new TreeNode(3);
+            root.right=new TreeNode(7);
+            root=new InsertIntoABinarySearchTree().InsertIntoBST(root, 5);
+
+            Assert.IsTrue(root.right.left.val==5);
+        }
+
+        [Test()]
+        public void InsertIntoBSTTest1()
+        {
+            TreeNode root = null;
+            root=new InsertIntoABinarySearchTree().InsertIntoBST(root, 5);
+
+            Assert.IsTrue(root.val==5);
+        }
+    }
+}
---
+++ src/LeetCode/BinarySearchTree/InsertIntoABinarySearchTree.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using LeetCode.BinaryTree;
+
+namespace LeetCode.BinarySearchTree
+{
+    /// <summary>
+    /// 701. 二叉搜索树中的插入操作
+    /// </summary>
+   public class InsertIntoABinarySearchTree
+    {
+        public TreeNode InsertIntoBST(TreeNode root, int val)
+        {
+            if (root == null)
+            {
+                root=new TreeNode(val);
+                return root;
+            }
+
+            TreeNode currentNode = root;
+            while (currentNode!=null)
+            {
+                if (currentNode.val > val)
+                {
+                    if (currentNode.left == null)
+                    {
+                        currentNode.left=new TreeNode(val);
+                        break;
+                    }
+                    else
+                    {
+                        currentNode = currentNode.left;
+                    }
+                }
+                else
+                {
+                    if (currentNode.right == null)
+                    {
+                        currentNode.right=new TreeNode(val);
+                        break;
+                    }
+                    else
+                    {
+                        currentNode = currentNode.right;
+                    }
+                }
+
+            }
+
+            return root;
+        }
+    }
+}
