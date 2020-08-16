7b97de654f10d471e3f0a699a2e7335e2e1d7a2e
Vincent <huangzihengengineer@gmail.com>
2020-08-16 19:11:10 +0800

    703. 数据流中的第K大元素

---
+++ src/LeetCode.Test/BinarySearchTree/KthLargestTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using LeetCode.BinarySearchTree;
+
+namespace LeetCode.Tests.BinarySearchTree
+{
+    [TestFixture()]
+    public class KthLargestTests
+    {
+        [Test()]
+        public void KthLargestTest()
+        {
+            int k = 3;
+            int[] nums = {4, 5, 8, 2};
+            KthLargest kthLargest = new KthLargest(k, nums);
+        }
+
+        [Test()]
+        public void AddTest()
+        {
+            int k = 3;
+            int[] arr = {4,5,8,2};
+            KthLargest kthLargest = new KthLargest(k, arr);
+           Assert.IsTrue(kthLargest.Add(3)==4);   // returns 4
+           Assert.IsTrue(kthLargest.Add(5)==5);   // returns 5
+           Assert.IsTrue(kthLargest.Add(10)==5);  // returns 5
+           Assert.IsTrue(kthLargest.Add(9)==8);   // returns 8
+           Assert.IsTrue(kthLargest.Add(4)==8);   // returns 8
+        }
+    }
+}
---
+++ src/LeetCode/BinarySearchTree/KthLargest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.BinarySearchTree
+{
+    /// <summary>
+    /// 703. 数据流中的第K大元素
+    /// </summary>
+    public class KthLargest
+    {
+        private int kth;
+        private LinkedList<int> nodeList = null;
+
+        public KthLargest(int k, int[] nums)
+        {
+            kth = k;
+            Array.Sort(nums);
+            nodeList=new LinkedList<int>(nums);
+        }
+       
+        public int Add(int val)
+        {
+            if (nodeList.Count == 0)
+            {
+                nodeList.AddFirst(val);
+            }
+            else if (nodeList.Last.Value <= val)
+            {
+                nodeList.AddLast(val);
+            }
+            else
+            {
+                // todo优化
+                int value = nodeList.First(u => u >= val);
+                var node = nodeList.Find(value);
+                nodeList.AddBefore(node, new LinkedListNode<int>(val));
+            }
+
+            //获取kth的值
+            LinkedListNode<int> currentNode = nodeList.Last;
+            for (int i = 0; i < kth-1; i++)
+            {
+                currentNode = currentNode.Previous;
+            }
+
+            return currentNode.Value;
+        }
+    }
+}
