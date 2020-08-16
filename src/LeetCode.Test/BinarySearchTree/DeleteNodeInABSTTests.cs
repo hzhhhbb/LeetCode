30a7bebfa1b0649c1528ab9fae8ef15656e0e421
Vincent <huangzihengengineer@gmail.com>
2020-08-16 16:00:44 +0800

    450. 删除二叉搜索树中的节点

---
+++ src/LeetCode.Test/BinarySearchTree/DeleteNodeInABSTTests.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+using LeetCode.BinarySearchTree;
+using LeetCode.BinaryTree;
+
+namespace LeetCode.Tests.BinarySearchTree
+{
+    [TestFixture()]
+    public class DeleteNodeInABSTTests
+    {
+        [Test()]
+        public void DeleteNodeTest()
+        {
+            //目标节点没有子节点
+            TreeNode root=new TreeNode(5);
+            root.left=new TreeNode(2);
+            root.left.left=new TreeNode(1);
+            root.left.right=new TreeNode(4);
+            root.left.right.left=new TreeNode(3);
+            root.right=new TreeNode(6);
+            root.right.right=new TreeNode(7);
+
+            int targetNodeValue = 7;
+            root=new DeleteNodeInABST().DeleteNode(root,targetNodeValue);
+            Assert.IsTrue(root.right.right == null);
+        }
+
+        [Test()]
+        public void DeleteNodeTest1()
+        {
+            //目标节点只有一个子节点
+            TreeNode root=new TreeNode(5);
+            root.left=new TreeNode(2);
+            root.left.left=new TreeNode(1);
+            root.left.right=new TreeNode(4);
+            root.left.right.left=new TreeNode(3);
+            root.right=new TreeNode(6);
+            root.right.right=new TreeNode(7);
+
+            int targetNodeValue = 6;
+            root=new DeleteNodeInABST().DeleteNode(root,targetNodeValue);
+            Assert.IsTrue(root.right.val == 7);
+        }
+
+        [Test()]
+        public void DeleteNodeTest2()
+        {
+            //目标节点有两个子节点
+            TreeNode root=new TreeNode(5);
+            root.left=new TreeNode(2);
+            root.left.left=new TreeNode(1);
+            root.left.right=new TreeNode(4);
+            root.left.right.left=new TreeNode(3);
+            root.right=new TreeNode(6);
+            root.right.right=new TreeNode(7);
+
+            int targetNodeValue = 2;
+            root=new DeleteNodeInABST().DeleteNode(root,targetNodeValue);
+            Assert.IsTrue(root.left.val==3);
+        }
+
+        [Test()]
+        public void DeleteNodeTest3()
+        {
+            //目标节点是根节点
+            TreeNode root=new TreeNode(5);
+            root.left=new TreeNode(2);
+            root.left.left=new TreeNode(1);
+            root.left.right=new TreeNode(4);
+            root.left.right.left=new TreeNode(3);
+            root.right=new TreeNode(6);
+            root.right.right=new TreeNode(7);
+
+            int targetNodeValue = 5;
+            root=new DeleteNodeInABST().DeleteNode(root,targetNodeValue);
+            Assert.IsTrue(root.val==6&&root.right.val==7&&root.left.val==2);
+        }
+    }
+}
---
+++ src/LeetCode/BinarySearchTree/DeleteNodeInABST.cs
@@ -0,0 +1,75 @@
+﻿using LeetCode.BinaryTree;
+
+namespace LeetCode.BinarySearchTree
+{
+    /// <summary>
+    /// 450. 删除二叉搜索树中的节点
+    /// </summary>
+    public class DeleteNodeInABST
+    {
+        public int GetSuccessorValue(TreeNode root)
+        {
+            root = root.right;
+            while (root.left != null)
+            {
+                root = root.left;
+            }
+            return root.val;
+        }
+
+        /*
+        One step left and then always right
+        */
+        public int GetPredecessorValue(TreeNode root)
+        {
+            root = root.left;
+            while (root.right != null)
+            {
+                root = root.right;
+            }
+            return root.val;
+        }
+
+        public TreeNode DeleteNode(TreeNode root, int key)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+
+            // delete from the right subtree
+            if (key > root.val)
+            {
+                root.right = DeleteNode(root.right, key);
+            }
+            // delete from the left subtree
+            else if (key < root.val)
+            {
+                root.left = DeleteNode(root.left, key);
+            }
+            // delete the current node
+            else
+            {
+                // the node is a leaf
+                if (root.left == null && root.right == null)
+                {
+                    root = null;
+                }
+                // the node is not a leaf and has a right child
+                else if (root.right != null)
+                {
+                    root.val = GetSuccessorValue(root);
+                    root.right = DeleteNode(root.right, root.val);
+                }
+                // the node is not a leaf, has no right child, and has a left child    
+                else
+                {
+                    root.val = GetPredecessorValue(root);
+                    root.left = DeleteNode(root.left, root.val);
+                }
+            }
+
+            return root;
+        }
+    }
+}
