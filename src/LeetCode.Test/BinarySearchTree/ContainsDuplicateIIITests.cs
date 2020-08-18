b399c316970f01a59ee529da0c099b536d59cc87
Vincent <huangzihengengineer@gmail.com>
2020-08-18 23:24:17 +0800

    220. 存在重复元素 III

---
+++ src/LeetCode.Test/BinarySearchTree/ContainsDuplicateIIITests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using LeetCode.BinarySearchTree;
+
+namespace LeetCode.Tests.BinarySearchTree
+{
+    [TestFixture()]
+    public class ContainsDuplicateIIITests
+    {
+        [Test()]
+        public void ContainsNearbyAlmostDuplicateTest()
+        {
+            int[] nums = {1, 2, 3, 1};
+            int k = 3;
+            int t = 0;
+            bool expected = true;
+            bool result = new ContainsDuplicateIII().ContainsNearbyAlmostDuplicate(nums, k, t);
+            Assert.AreEqual(expected,result);
+        }
+
+        [Test()]
+        public void ContainsNearbyAlmostDuplicateTest1()
+        {
+            int[] nums = {1,0,1,1};
+            int k =1;
+            int t = 2;
+            bool expected = true;
+            bool result = new ContainsDuplicateIII().ContainsNearbyAlmostDuplicate(nums, k, t);
+            Assert.AreEqual(expected,result);
+        }
+
+        [Test()]
+        public void ContainsNearbyAlmostDuplicateTest2()
+        {
+            int[] nums = {1,5,9,1,5,9};
+            int k = 2;
+            int t = 3;
+
+            bool expected = false;
+            bool result = new ContainsDuplicateIII().ContainsNearbyAlmostDuplicate(nums, k, t);
+            Assert.AreEqual(expected,result);
+        }
+
+        [Test()]
+        public void ContainsNearbyAlmostDuplicateTest3()
+        {
+            int[] nums = {7,2,8};
+            int k = 2;
+            int t = 1;
+
+            bool expected = true;
+            bool result = new ContainsDuplicateIII().ContainsNearbyAlmostDuplicate(nums, k, t);
+            Assert.AreEqual(expected,result);
+        }
+    }
+}
---
+++ src/LeetCode/BinarySearchTree/ContainsDuplicateIII.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.BinarySearchTree
+{
+    /// <summary>
+    /// 220. 存在重复元素 III（超出时间限制）
+    /// </summary>
+    public class ContainsDuplicateIII
+    {
+        public bool ContainsNearbyAlmostDuplicate(int[] nums, int k, int t)
+        {
+            SortedSet<long?> set = new SortedSet<long?>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                //滑动窗口内有大于当前元素的第一个值、并且它们的差小于等于t
+                // O(N)复杂度
+                var successor = set.FirstOrDefault(u => u >= nums[i]);
+                if (successor!=null && successor - nums[i] <= t)
+                {
+                    return true;
+                }
+
+                //滑动窗口内有小于当前元素的第一个值、并且它们的差小于等于t
+                // O(N)复杂度
+                var predecessor = set.LastOrDefault(u => u <= nums[i]);
+                if (predecessor!=null && nums[i] - predecessor <= t)
+                {
+                    return true;
+                }
+
+                set.Add(nums[i]);
+
+                //维持set的大小在k大小的滑动窗口内
+                if (set.Count > k)
+                {
+                    set.Remove(nums[i - k]);
+                }
+            }
+
+            return false;
+        }
+
+        /* 使用平衡二叉搜索树
+         * 在k大小的滑动窗口内，寻找是否存在num[i]-nums[j]的绝对值小于等于t的情况即可
+         */
+    }
+}
