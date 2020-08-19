596164fc42b55740453b94cfc192e1456bcde3c4
Vincent <huangzihengengineer@gmail.com>
2020-08-19 21:24:14 +0800

    108. 将有序数组转换为二叉搜索树

---
+++ src/LeetCode.Test/BinarySearchTree/ConvertSortedArrayToBinarySearchTreeTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using LeetCode.BinarySearchTree;
+using LeetCode.BinaryTree;
+
+namespace LeetCode.Tests.BinarySearchTree
+{
+    [TestFixture()]
+    public class ConvertSortedArrayToBinarySearchTreeTests
+    {
+        [Test()]
+        public void SortedArrayToBSTTest()
+        {
+            int[] nums = {-10, -3, 0, 5, 9};
+            TreeNode root=new TreeNode(0);
+            root.left=new TreeNode(-10);
+            root.left.right=new TreeNode(-3);
+            root.right=new TreeNode(5);
+            root.right.right=new TreeNode(9);
+            
+            TreeNode result=new ConvertSortedArrayToBinarySearchTree().SortedArrayToBST(nums);
+            Assert.IsTrue(root.val==result.val);
+            Assert.IsTrue(root.left.right.val==result.left.right.val);
+            Assert.IsTrue(root.right.val==result.right.val);
+            Assert.IsTrue(root.right.right.val==result.right.right.val);
+        }
+    }
+}
---
+++ src/LeetCode/BinarySearchTree/ConvertSortedArrayToBinarySearchTree.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode.BinaryTree;
+
+namespace LeetCode.BinarySearchTree
+{
+    /// <summary>
+    /// 108. 将有序数组转换为二叉搜索树
+    /// </summary>
+   public class ConvertSortedArrayToBinarySearchTree
+    {
+        public TreeNode SortedArrayToBST(int[] nums) 
+        {
+            if (nums.Length == 0)
+            {
+                return null;
+            }
+
+            if (nums.Length == 1)
+            {
+                return new TreeNode(nums[0]);
+            }
+
+
+            return SortedArrayToBST(nums, 0, nums.Length - 1);
+        }
+
+        private TreeNode SortedArrayToBST(int[] nums, int left, int right)
+        {
+            if (left > right)
+            {
+                return null;
+            }
+
+            int mid = (left + right) / 2;
+            TreeNode root=new TreeNode(nums[mid]);
+            root.left = SortedArrayToBST(nums, left, mid-1);
+            root.right = SortedArrayToBST(nums, mid + 1, right);
+            return root;
+        }
+
+    }
+}
