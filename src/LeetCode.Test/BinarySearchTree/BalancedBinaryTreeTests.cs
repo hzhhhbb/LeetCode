1546a1471b696964e68de8fe0ea64eb1e815a730
Vincent <huangzihengengineer@gmail.com>
2020-08-19 20:33:06 +0800

    110. 平衡二叉树

---
+++ src/LeetCode.Test/BinarySearchTree/BalancedBinaryTreeTests.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode.Tests.BinarySearchTree
+{
+    using LeetCode.BinarySearchTree;
+    using LeetCode.BinaryTree;
+
+    using NUnit.Framework;
+
+    [TestFixture()]
+    public class BalancedBinaryTreeTests
+    {
+        [Test()]
+        public void IsBalancedTest()
+        {
+            // balanced tree
+            TreeNode root=new TreeNode(3);
+            root.left=new TreeNode(9);
+            root.right=new TreeNode(20);
+            root.right.left=new TreeNode(15);
+            root.right.right=new TreeNode(7);
+
+            bool expected = true;
+            bool isBalanced = new BalancedBinaryTree().IsBalanced(root);
+            Assert.AreEqual(expected,isBalanced);
+        }
+
+        [Test()]
+        public void IsBalancedTest1()
+        {
+            // not  balanced tree
+            TreeNode root=new TreeNode(3);
+            root.left=new TreeNode(9);
+            root.right=new TreeNode(20);
+            root.right.left=new TreeNode(15);
+            root.right.right=new TreeNode(7);
+            root.right.right.left=new TreeNode(1);
+            root.right.right.left.left=new TreeNode(1);
+            bool expected = false;
+            bool isBalanced = new BalancedBinaryTree().IsBalanced(root);
+            Assert.AreEqual(expected,isBalanced);
+        }
+    }
+}
---
+++ src/LeetCode/BinarySearchTree/BalancedBinaryTree.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.BinarySearchTree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using LeetCode.BinaryTree;
+
+    /// <summary>
+    /// 110. 平衡二叉树
+    /// </summary>
+    public class BalancedBinaryTree
+    {
+        public bool IsBalanced(TreeNode root)
+        {
+            if (root == null)
+            {
+                return true;
+            }
+            else
+            {
+                return Math.Abs(Height(root.left) - Height(root.right)) <= 1 && IsBalanced(root.left) &&
+                       IsBalanced(root.right);
+            }
+        }
+
+        //空节点高度为0，否则为1，递归求高度即可
+        public int Height(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return Math.Max(Height(root.left), Height(root.right)) + 1;
+            }
+        }
+    }
+}
