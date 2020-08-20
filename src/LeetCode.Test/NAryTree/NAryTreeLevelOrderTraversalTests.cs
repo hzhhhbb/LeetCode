b996c36f8bbf881e395952c8ad7184d0cb9b815f
Vincent <huangzihengengineer@gmail.com>
2020-08-20 21:19:57 +0800

    102. 二叉树的层序遍历

---
+++ src/LeetCode.Test/NAryTree/NAryTreeLevelOrderTraversalTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using LeetCode.NAryTree;
+using System.Collections.Generic;
+
+namespace LeetCode.Tests.NAryTree
+{
+    [TestFixture()]
+    public class NAryTreeLevelOrderTraversalTests
+    {
+        [Test()]
+        public void LevelOrderTest()
+        {
+            Node root=new Node(1,new List<Node>(){new Node(3,new List<Node>(){new Node(5),new Node(6)}),new Node(2),new Node(4)});
+            IList<IList<int>> expected=new List<IList<int>>()
+            {
+                new List<int>(){1},
+                new List<int>(){3,2,4},
+                new List<int>(){5,6},
+            };
+
+            var result = new NAryTreeLevelOrderTraversal().LevelOrder(root);
+            Assert.AreEqual(expected,result);
+        }
+
+        [Test()]
+        public void LevelOrderTest1()
+        {
+            Node root=new Node(1,new List<Node>(){new Node(3),new Node(2,new List<Node>(){new Node(5),new Node(6)}),new Node(4)});
+            IList<IList<int>> expected=new List<IList<int>>()
+            {
+                new List<int>(){1},
+                new List<int>(){3,2,4},
+                new List<int>(){5,6},
+            };
+
+            var result = new NAryTreeLevelOrderTraversal().LevelOrder(root);
+            Assert.AreEqual(expected,result);
+        }
+    }
+}
---
+++ src/LeetCode/NAryTree/NAryTreeLevelOrderTraversal.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.NAryTree
+{
+    /// <summary>
+    /// 429. N叉树的层序遍历
+    /// </summary>
+    public class NAryTreeLevelOrderTraversal
+    {
+        public IList<IList<int>> LevelOrder(Node root)
+        {
+            IList<IList<int>> result=new List<IList<int>>();
+            if (root == null)
+            {
+                return result;
+            }
+
+            Queue<Node> queue=new Queue<Node>();
+            queue.Enqueue(root);
+          
+            while (queue.Any())
+            {
+                int count = queue.Count;
+                IList<int> levelNode=new List<int>(count);
+                for (int i = 0; i < count; i++)
+                {
+                    var node = queue.Dequeue();
+                    levelNode.Add(node.val);
+                    if (node.children != null)
+                    {
+                        foreach (Node child in node.children)
+                        {
+                            queue.Enqueue(child);
+                        }
+                    }
+                }
+
+                result.Add(levelNode);
+            }
+
+            return result;
+        }
+    }
+}
