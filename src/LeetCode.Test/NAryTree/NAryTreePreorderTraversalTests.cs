0d7019b81873f82f00c8a46ec968e6716f68da65
Vincent <huangzihengengineer@gmail.com>
2020-08-20 19:58:19 +0800

    589. N叉树的前序遍历

---
+++ src/LeetCode.Test/NAryTree/NAryTreePreorderTraversalTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using LeetCode.NAryTree;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Tests.BinarySearchTree
+{
+    [TestFixture()]
+    public class NAryTreePreorderTraversalTests
+    {
+        [Test()]
+        public void PreorderTest()
+        {
+            //递归法
+            Node root=new Node(1,new List<Node>(){new Node(3,new List<Node>(){new Node(5),new Node(6)}),new Node(2),new Node(4)});
+            IList<int> expected=new List<int>(){1,3,5,6,2,4};
+            var result = new NAryTreePreorderTraversal().Preorder(root);
+            Assert.AreEqual(expected,result);
+        }
+
+        [Test()]
+        public void PreorderTest1()
+        {
+            //递归法
+            Node root = null;
+            IList<int> expected = new List<int>();
+            var result = new NAryTreePreorderTraversal().Preorder(root);
+            Assert.AreEqual(expected,result);
+        }
+
+        [Test()]
+        public void PreorderTest2()
+        {
+            //迭代法
+            Node root=new Node(1,new List<Node>(){new Node(3,new List<Node>(){new Node(5),new Node(6)}),new Node(2),new Node(4)});
+            IList<int> expected=new List<int>(){1,3,5,6,2,4};
+            var result = new NAryTreePreorderTraversal().Preorder1(root);
+            Assert.AreEqual(expected,result);
+        }
+
+        [Test()]
+        public void PreorderTest3()
+        {
+            //迭代法
+            Node root = null;
+            IList<int> expected = new List<int>();
+            var result = new NAryTreePreorderTraversal().Preorder1(root);
+            Assert.AreEqual(expected,result);
+        }
+    }
+}
---
+++ src/LeetCode/NAryTree/NAryTreePreorderTraversal.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.NAryTree
+{
+    /// <summary>
+    /// 589. N叉树的前序遍历
+    /// </summary>
+    public class NAryTreePreorderTraversal
+    {
+        //递归法
+        public IList<int> Preorder(Node root)
+        {
+            var result = new List<int>();
+            if (root == null)
+            {
+                return result;
+            }
+
+            result.Add(root.val);
+
+            if (root.children != null && root.children.Any())
+            {
+                foreach (Node node in root.children)
+                {
+                    result.AddRange(this.Preorder(node));
+                }
+            }
+
+            return result;
+        }
+
+        //迭代法
+        public IList<int> Preorder1(Node root)
+        {
+            var result = new List<int>();
+            if (root == null)
+            {
+                return result;
+            }
+
+            Stack<Node> stack = new Stack<Node>();
+            stack.Push(root);
+            while (stack.Any())
+            {
+                var node = stack.Pop();
+                result.Add(node.val);
+                if (node.children != null && node.children.Any())
+                {
+                    for (int i = node.children.Count - 1; i >= 0; i--)
+                    {
+                        stack.Push(node.children[i]);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
