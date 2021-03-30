40755e00edcc3bdbf04eccbbd9b3a346e7722c9d
Vincent <huangzihengengineer@gmail.com>
2021-03-30 23:12:42 +0800

    74. 搜索二维矩阵

---
+++ src/LeetCode.Test/StringAndArray/SearchMatrixClass_Tests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using LeetCode.StringAndArray;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Shouldly;
+
+namespace LeetCode.Tests.StringAndArray
+{
+    [TestFixture()]
+    public class SearchMatrixClass_Tests
+    {
+        [Test()]
+        [Timeout(1000)]
+        public void SearchMatrix_Test()
+        {
+           int [][] matrix =new int[][]
+           {
+               new int[]{1,3,5,7,8}, 
+               new int[]{10,11,16,20,10}, 
+               new int[]{23,30,34,60,70}, 
+           };
+
+           new SearchMatrixClass().SearchMatrix(matrix,10).ShouldBeTrue();
+           new SearchMatrixClass().SearchMatrix(matrix,11).ShouldBeTrue();
+           new SearchMatrixClass().SearchMatrix(matrix,16).ShouldBeTrue();
+           new SearchMatrixClass().SearchMatrix(matrix,23).ShouldBeTrue();
+           new SearchMatrixClass().SearchMatrix(matrix,60).ShouldBeTrue();
+           new SearchMatrixClass().SearchMatrix(matrix,70).ShouldBeTrue();
+           new SearchMatrixClass().SearchMatrix(matrix,23).ShouldBeTrue();
+           new SearchMatrixClass().SearchMatrix(matrix,1).ShouldBeTrue();
+           new SearchMatrixClass().SearchMatrix(matrix,7).ShouldBeTrue();
+           new SearchMatrixClass().SearchMatrix(matrix,10).ShouldBeTrue();
+           new SearchMatrixClass().SearchMatrix(matrix,3).ShouldBeTrue();
+           new SearchMatrixClass().SearchMatrix(matrix,7).ShouldBeTrue();
+           new SearchMatrixClass().SearchMatrix(matrix,34).ShouldBeTrue();
+           new SearchMatrixClass().SearchMatrix(matrix,35).ShouldBeFalse();
+        }
+    }
+}
---
+++ src/LeetCode/StringAndArray/SearchMatrixClass.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.StringAndArray
+{
+    /// <summary>
+    /// 74. 搜索二维矩阵
+    /// </summary>
+    public class SearchMatrixClass
+    {
+
+        /*
+         * 因为二位数据都是有序的，并且行有限，列很多
+         * 第一步：先找到数据所在的行
+         * 第二步：通过二分查找在行中查找目标值
+         */
+
+        public bool SearchMatrix(int[][] matrix, int target)
+        {
+            if (!matrix.Any())
+            {
+                return false;
+            }
+
+            if (target < -10000 || target > 10000)
+            {
+                return false;
+            }
+
+            int lineNumber = 0;
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                if (matrix[i][0] > target)
+                {
+                    break;
+                }
+
+                lineNumber = i;
+            }
+            
+            int left = 0;
+            int right = matrix[lineNumber].Length-1;
+            int mid = 0;
+
+            while (left<=right)
+            {
+                mid = left + (right - left) / 2;
+                if (matrix[lineNumber][mid] == target)
+                {
+                    return true;
+                }
+
+                if (matrix[lineNumber][mid] < target)
+                {
+                    left = mid+1;
+                }
+                else
+                {
+                    right = mid-1;
+                }
+            }
+
+            return false;
+        }
+    }
+}
