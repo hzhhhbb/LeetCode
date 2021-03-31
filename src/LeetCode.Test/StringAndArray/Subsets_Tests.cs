9327be9144d757f5a38ef068ac12d3aad8413457
vincent <huangzihengengineer@gmail.com>
2021-03-31 18:08:42 +0800

    sub set

---
+++ src/LeetCode.Test/StringAndArray/Subsets_Tests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using LeetCode.StringAndArray;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Shouldly;
+
+namespace LeetCode.StringAndArray.Tests
+{
+    [TestFixture()]
+    public class Subsets_Tests
+    {
+        [Test()]
+        public void SubsetsWithDup_Test()
+        {
+
+            int[] nums =new int[]{1};
+            new Subsets().SubsetsWithDup(nums).Count.ShouldBe(2);
+
+            nums=new int[]{};
+            new Subsets().SubsetsWithDup(nums).Count.ShouldBe(1);
+
+
+            nums = new int[]{1, 2, 3};
+            new Subsets().SubsetsWithDup(nums).Count.ShouldBe(8);
+
+            nums = new int[]{1, 2, 2};
+            new Subsets().SubsetsWithDup(nums).Count.ShouldBe(6);
+
+            nums = new int[]{1, 2, 2,3};
+            new Subsets().SubsetsWithDup(nums).Count.ShouldBe(12);
+
+            nums =  new int[]{1, 2, 3,4,5};
+            new Subsets().SubsetsWithDup(nums).Count.ShouldBe(32);
+        }
+    }
+}
---
+++ src/LeetCode/StringAndArray/Subsets.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.StringAndArray
+{
+    public class Subsets
+    {
+        public IList<IList<int>> SubsetsWithDup(int[] nums)
+        {
+            // 升序排序
+            Array.Sort(nums);
+
+            IList<IList<int>> result=new List<IList<int>>();
+
+            result.Add(new List<int>());
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var newSub = new List<IList<int>>(result);
+
+                foreach (var u in newSub)
+                {
+                    var temp = new List<int>(u);
+                    temp.Add(nums[i]);
+
+                    bool exists = false;
+
+                    foreach (IList<int> list in result)
+                    {
+                        if (list.Count == temp.Count)
+                        {
+                            bool tempExists = true;
+                            for (int j = 0; j < temp.Count; j++)
+                            {
+                                if (list[j] != temp[j])
+                                {
+                                    tempExists=false;
+                                    break;
+                                }
+                            }
+
+                            if (tempExists)
+                            {
+                                exists = true;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (!exists)
+                    {
+                        result.Add(temp);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
